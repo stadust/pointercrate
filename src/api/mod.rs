644880964ccecf8bff3e724d57a5e6904fd756d3
stadust <43299462+stadust@users.noreply.github.com>
2018-12-30 20:40:44 +0100

    Wrote a kinda nicer thing for producing correct error responses
    also rustfmt takes great pride in not formatting src/api/mod.rs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -5,28 +5,26 @@ pub mod record;
 pub mod user;
 
 use crate::{
-    error::{PointercrateError, PointercrateErrorResponse},
+    error::PointercrateError,
     middleware::mime::Accept,
     state::PointercrateState,
     view::{error::ErrorPage, Page},
 };
-use actix_web::{HttpRequest, HttpResponse};
+use actix_web::{error::ResponseError, HttpRequest, HttpResponse};
 use mime;
-use tokio::prelude::future::{Future, IntoFuture};
+use tokio::prelude::future::{err, Either, Future};
 
 pub type PCResponder = Box<dyn Future<Item = HttpResponse, Error = PointercrateError>>;
-
-// FIXME: This is kinda horrible
 
 pub fn wrap<F>(
     handler: F,
 ) -> impl Fn(
     &HttpRequest<PointercrateState>,
-) -> Box<dyn Future<Item = HttpResponse, Error = PointercrateErrorResponse>>
+) -> Box<dyn Future<Item = HttpResponse, Error = PointercrateError>>
 where
     F: Fn(&HttpRequest<PointercrateState>) -> PCResponder + 'static,
 {
-    move |req: &HttpRequest<PointercrateState>| -> Box<dyn Future<Item = HttpResponse, Error = PointercrateErrorResponse>> {
+    move |req: &HttpRequest<PointercrateState>| -> Box<dyn Future<Item = HttpResponse, Error = PointercrateError>> {
         let Accept(accepted) = req.extensions_mut().remove().unwrap();
 
         let (preference, mime_type) = accepted
@@ -45,21 +43,26 @@ where
                     mime,
                 )
             })
-            .max_by_key(|(q, _)| (q * 100.0) as i32)
+            .max_by_key(|(q, _)| (q * 100.0) as i32)  // cannot compare floats dammit
             .unwrap_or((1.0, mime::TEXT_HTML));
 
-        if preference < 0.0 || preference > 1.0 {
-            Box::new(Err(PointercrateErrorResponse::Default(PointercrateError::InvalidHeaderValue { header: "Accept" }))
-                .into_future())
+        let thing = if preference < 0.0 || preference > 1.0 {
+            Either::A(err::<HttpResponse, _>(PointercrateError::InvalidHeaderValue { header: "Accept" }))
         } else {
             let req_clone = req.clone();
+            let f = handler(req).or_else(move |error| Ok(match (mime_type.type_(), mime_type.subtype()) {
+                (mime::TEXT, mime::HTML)  => {
+                    let html = ErrorPage::new(&error).render(&req_clone);
+                    HttpResponse::Ok()
+                        .content_type("text/html; charset=utf-8")
+                        .body(html.0)
+                },
+                (mime::APPLICATION, mime::JSON) => error.error_response(),
+                _ => unreachable!()
+            }));
+            Either::B(f)
+        };
 
-            Box::new(handler(req).map_err(move |pcerr| {
-                match (mime_type.type_(), mime_type.subtype()) {
-                    (mime::TEXT, mime::HTML) => PointercrateErrorResponse::Html(ErrorPage::new(&pcerr).render(&req_clone)),
-                    _ => PointercrateErrorResponse::Default(pcerr),
-                }
-            }))
-        }
+        Box::new(thing)
     }
 }
