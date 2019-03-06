fa978391e9122dc695ce76159a303e7b096f2c22
poisontbh <41485054+poisontbh@users.noreply.github.com>
2019-03-06 13:40:11 +0000

    replace list_size with 100

--- sql/ranking.sql
+++ sql/ranking.sql
@@ -1,11 +1,7 @@
-
-WITH aux AS (
-    SELECT COUNT(*)::FLOAT AS list_size FROM demons
-)
 SELECT RANK() OVER(ORDER BY t.total_score DESC) as rank, t.total_score as score, players.name as name, t.player as id
 FROM
 (
-    SELECT a.player, SUM(record_score(a.progress::FLOAT, a.position::FLOAT, (SELECT list_size FROM aux))) as total_score
+    SELECT a.player, SUM(record_score(a.progress::FLOAT, a.position::FLOAT, (SELECT 100 FROM aux))) as total_score
     FROM (
         SELECT player as player, progress as progress, demons.position as position
         FROM records
@@ -23,4 +19,4 @@ FROM
 ) t
 INNER JOIN players
 ON t.player = players.id
-WHERE t.total_score >= record_score(100.0::FLOAT, {0}::FLOAT, (SELECT list_size FROM aux));
+WHERE t.total_score >= record_score(100.0::FLOAT, {0}::FLOAT, (SELECT 100 FROM aux));
