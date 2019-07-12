45f90d888316966aff589ce4036f4d572492d67c
stadust <43299462+stadust@users.noreply.github.com>
2019-07-12 18:11:17 +0200

    or like more than one fucked up merge the actual fuck happened here

--- migrations/2019-03-12-131253_nationality/down.sql
+++ migrations/2019-03-12-131253_nationality/down.sql
@@ -3,8 +3,4 @@
 ALTER TABLE players DROP COLUMN nationality;
 ALTER TABLE members DROP COLUMN nationality;
 
-<<<<<<< HEAD
 DROP TABLE nationalities;
-=======
-DROP TABLE nationality;
->>>>>>> 5b6423a... Essentially basic nationality support
