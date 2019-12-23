13417084eae31e489c9914258ab9138058e8f1b0
stadust <43299462+stadust@users.noreply.github.com>
2019-12-23 15:39:19 +0100

    Fix usage of diesel as mentioned in #30

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -25,45 +25,44 @@ pub use self::user::User;
 
 use diesel::{
     dsl::{Eq, Filter, Select},
-    expression::{AsExpression, Expression, SelectableExpression},
+    expression::{AsExpression, Expression},
+    helper_types::IntoBoxed,
     pg::Pg,
-    query_builder::{BoxedSelectStatement, QueryFragment, SelectStatement},
     query_dsl::{boxed_dsl::BoxedDsl, filter_dsl::FilterDsl, select_dsl::SelectDsl},
-    query_source::Column,
-    ExpressionMethods, QuerySource,
+    ExpressionMethods,
 };
 
-pub type All<M> = Select<SelectStatement<<M as Model>::From>, <M as Model>::Selection>;
+pub type All<M> = Select<<M as Model>::From, <M as Model>::Selection>;
 
 pub trait Model {
-    type From: QuerySource;
-    type Selection: Expression + SelectableExpression<Self::From> + QueryFragment<Pg> + 'static;
+    type From: SelectDsl<Self::Selection>;
+    type Selection: Expression;
 
     fn from() -> Self::From;
 
     fn selection() -> Self::Selection;
 
-    fn all() -> Select<SelectStatement<Self::From>, Self::Selection> {
-        SelectStatement::simple(Self::from()).select(Self::selection())
+    fn all() -> Select<Self::From, Self::Selection> {
+        SelectDsl::select(Self::from(), Self::selection())
     }
 
-    fn boxed_all<'a>(
-    ) -> BoxedSelectStatement<'a, <Self::Selection as Expression>::SqlType, Self::From, Pg> {
-        BoxedDsl::internal_into_boxed(
-            SelectStatement::simple(Self::from()).select(Self::selection()),
-        )
+    fn boxed_all() -> IntoBoxed<'static, All<Self>, Pg>
+    where
+        All<Self>: BoxedDsl<'static, Pg>,
+    {
+        Self::all().internal_into_boxed()
     }
 }
 
-trait By<T: Column + Default + ExpressionMethods, U: AsExpression<T::SqlType>>: Model {
-    fn with(u: U) -> diesel::dsl::Eq<T, U> {
-        T::default().eq(u)
-    }
-
+trait By<T, U>: Model
+where
+    T: Default + ExpressionMethods,
+    U: AsExpression<T::SqlType>,
+{
     fn by(u: U) -> Filter<All<Self>, Eq<T, U>>
     where
         All<Self>: FilterDsl<Eq<T, U>>,
     {
-        Self::all().filter(Self::with(u))
+        Self::all().filter(T::default().eq(u))
     }
 }
