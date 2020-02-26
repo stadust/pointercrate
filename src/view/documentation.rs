a194a98263bf2ae3c9091abbd91eae30318987c5
stadust <43299462+stadust@users.noreply.github.com>
2020-02-26 15:15:06 +0100

    adding files the the commit is hard

---
+++ src/view/documentation.rs
@@ -0,0 +1,131 @@
+use super::Page;
+use crate::{error::PointercrateError, state::PointercrateState, Result};
+use actix_web::{web::Path, HttpRequest, HttpResponse};
+use actix_web_codegen::get;
+use maud::{html, Markup, PreEscaped, Render};
+
+#[derive(Debug)]
+pub struct Documentation<'a> {
+    toc: &'a str,
+    content: &'a str,
+    page: &'a str,
+}
+
+impl<'a> Documentation<'a> {
+    pub fn new(state: &'a PointercrateState, page: &'a str) -> Result<Documentation<'a>> {
+        let content = match state.documentation_topics.get(page) {
+            Some(cnt) => cnt,
+            _ => return Err(PointercrateError::NotFound),
+        };
+
+        Ok(Documentation {
+            toc: &*state.documentation_toc,
+            content,
+            page,
+        })
+    }
+}
+
+// actix complains if these aren't async, although they don't not have to be
+#[get("/documentation/")]
+pub async fn index(state: PointercrateState) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(Documentation::new(&state, "index")?.render().0))
+}
+
+#[get("/documentation/{topic}/")]
+pub async fn topic(state: PointercrateState, topic: Path<String>) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(Documentation::new(&state, &topic.into_inner())?.render().0))
+}
+
+impl<'a> Page for Documentation<'a> {
+    fn title(&self) -> String {
+        format!("API Documentation - {}", self.page)
+    }
+
+    fn description(&self) -> String {
+        "The pointercrate API, which allows you to programmatically interface with the Demonlist".to_owned()
+    }
+
+    fn scripts(&self) -> Vec<&str> {
+        vec![]
+    }
+
+    fn stylesheets(&self) -> Vec<&str> {
+        vec!["css/sidebar.css", "css/doc.css"]
+    }
+
+    fn body(&self) -> Markup {
+        html! {
+            div class="m-center flex container" {
+                div.left {
+                    (PreEscaped(self.content))
+                }
+                div.right {
+                    (PreEscaped(self.toc))
+                }
+            }
+            (PreEscaped(r#"
+                <script>
+                // you know, this might be the most ugly solution to a problem I have ever thought of
+                $(document).ready(function() {
+                  $("h1").append(
+                    '<a class="fa fa-link fa-3 link-anchor" aria-hidden="true" title="Permanent link to this topic"></a>'
+                  );
+                  $("h1").prepend(
+                    '<i class="fa fa-link fa-3 link-anchor" style="visibility:hidden" aria-hidden="true"></i>'
+                  );
+                  $(".link-anchor").each((idx, elem) =>
+                    $(elem).attr("href", '#' + $(elem).parent()[0].id)
+                  );
+                })
+                </script>
+            "#))
+        }
+    }
+
+    fn head(&self) -> Vec<Markup> {
+        vec![html! {
+            (PreEscaped(r#"
+<script type="application/ld+json">
+  {
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    "breadcrumb": {
+      "@type": "BreadcrumbList",
+      "itemListElement": [{
+        "@type": "ListItem",
+        "position": 1,
+        "item": {
+          "@id": "https://pointercrate.com/",
+          "name": "pointercrate"
+        }
+      },{
+        "@type": "ListItem",
+        "position": 2,
+        "item": {
+          "@id": "https://pointercrate.com/documentation/",
+          "name": "documentation"
+        }
+      },{
+        "@type": "ListItem",
+        "position": 3,
+        "item": {
+            "@id": "https://pointercrate.com/documentation/account/",
+            "name": "account"
+        }
+      }]
+    },
+    "name": "API Documentation",
+    "description": "The pointercrate API, which allows you to programmatically interface with the Demonlist",
+    "url": "https://pointercrate.com/documentation/account/",
+    "dateCreated": "2017-04-08"
+  }
+</script>
+            "#))
+        }]
+    }
+}
---
+++ src/view/login.rs
@@ -0,0 +1,128 @@
+use super::Page;
+use crate::{
+    extractor::auth::{BasicAuth, TokenAuth},
+    state::PointercrateState,
+    Result,
+};
+use actix_web::{cookie::SameSite, http::Cookie, HttpRequest, HttpResponse, Responder};
+use actix_web_codegen::{get, post};
+use log::info;
+use maud::{html, Markup};
+
+#[derive(Debug, Copy, Clone)]
+pub struct LoginPage;
+
+#[get("/login/")]
+pub fn index(user: Result<TokenAuth>) -> HttpResponse {
+    match user {
+        Ok(user) => HttpResponse::Found().header("Location", "/account/").finish(),
+        _ =>
+            HttpResponse::Ok()
+                .content_type("text/html; charset=utf-8")
+                .body(LoginPage.render().0),
+    }
+}
+
+/// Alternate login handler for the web interface. Unlike the one in the api, it doesn't return your
+/// token, but puts it into a secure, http-only cookie
+#[post("/login/")]
+pub fn post(BasicAuth(user): BasicAuth, state: PointercrateState) -> HttpResponse {
+    info!("POST /login/");
+
+    let mut cookie = Cookie::build("access_token", user.generate_token(&state.secret))
+        .http_only(true)
+        .same_site(SameSite::Strict)
+        .path("/");
+
+    // allow cookies of HTTP if we're in a debug build, because I don't have a ssl cert for
+    // 127.0.0.1 on my laptop smh
+    if !cfg!(debug_assertions) {
+        cookie = cookie.secure(true)
+    }
+
+    HttpResponse::NoContent().cookie(cookie.finish()).finish()
+}
+
+impl Page for LoginPage {
+    fn title(&self) -> String {
+        "Pointercrate - Login".to_string()
+    }
+
+    fn description(&self) -> String {
+        "Log in to an existing pointercrate account or register for a new one!".to_string()
+    }
+
+    fn scripts(&self) -> Vec<&str> {
+        vec!["js/login.js", "js/form.js"]
+    }
+
+    fn stylesheets(&self) -> Vec<&str> {
+        vec!["css/login.css"]
+    }
+
+    fn body(&self) -> Markup {
+        html! {
+            div.m-center.flex.panel.fade.col.wrap style = "margin: 100px 0px;"{
+                h1.underlined.pad {
+                    "Pointercrate Account"
+                }
+                p {
+                    "By using pointercrate accounts you agree to cookies. If you don't then I formally request you to stop using the internet as you obviously have no idea what you're talking about. "
+                }
+                div.flex#login {
+                    div.flex.col {
+                        h2 {"Login"}
+                        p {
+                            "Log in to an existing pointercrate account. You have 3 login attempts by 30 minutes. If you do not have an account yet, register on the right or below. "
+                        }
+                        form.flex.col.grow#login-form novalidate = "" {
+                            p.info-red.output {}
+                            span.form-input#login-username {
+                                label for = "username" {"Username:"}
+                                input required = "" type = "text" name = "username" minlength = "3";
+                                p.error {}
+                            }
+                            span.form-input#login-password {
+                                label for = "password" {"Password:"}
+                                input required = "" type = "password" name = "password" minlength = "10";
+                                p.error {}
+                            }
+                            div.grow {}
+                            input.button.blue.hover.slightly-round type = "submit" style = "margin: 15px auto 0px;" value="Log in";
+                        }
+                    }
+                    div.flex.col {
+                        h2 {"Register"}
+                        p {
+                            "Not registered yet? Create a new pointercrate account below."
+                        }
+                        form.flex.col.grow#register-form novalidate = "" {
+                            p.info-red.output {}
+                            span.form-input#register-username {
+                                label for = "username" {"Username:"}
+                                input required = "" type = "text" name = "username";
+                                p.error {}
+                            }
+                            span.form-input#register-password {
+                                label for = "password" {"Password:"}
+                                input required = "" type = "password" name = "password" minlength = "10";
+                                p.error {}
+                            }
+                            span.form-input#register-password-repeat {
+                                label for = "password2" {"Repeat Password:"}
+                                input required = "" type = "password" name = "password2" minlength = "10";
+                                p.error {}
+                            }
+                            div.grow {}
+                            input.button.blue.hover.slightly-round type = "submit" style = "margin: 15px auto 0px;" value = "Register";
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn head(&self) -> Vec<Markup> {
+        vec![]
+    }
+}
