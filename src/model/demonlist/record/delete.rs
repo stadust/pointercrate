abee8db7fa7a4c93b420c9d09b09c68d0294208b
stadust <43299462+stadust@users.noreply.github.com>
2020-02-29 19:22:19 +0100

    fixed record deletion failing when there are notes on the record
    this caused automatic deletation of invalid submissions to fail, if the submitter left a note

--- src/model/demonlist/record/delete.rs
+++ src/model/demonlist/record/delete.rs
@@ -6,6 +6,9 @@ impl FullRecord {
     pub async fn delete(self, connection: &mut PgConnection) -> Result<()> {
         info!("Deleting record {}", self);
 
+        sqlx::query!("DELETE FROM record_notes WHERE record = $1", self.id)
+            .execute(connection)
+            .await?;
         sqlx::query!("DELETE FROM records WHERE id = $1", self.id)
             .execute(connection)
             .await?;
