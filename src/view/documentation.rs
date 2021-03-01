958d1fa0b45dc886b5c429f44f52f473c9d2c95b
NimbusGD <71958627+NimbusGD@users.noreply.github.com>
2021-03-01 08:15:17 -0600

    Update documentation.rs

--- src/view/documentation.rs
+++ src/view/documentation.rs
@@ -9,10 +9,12 @@ pub struct Documentation<'a> {
     toc: &'a str,
     content: &'a str,
     page: &'a str,
+    description: &'static str,
+    title: &'static str,
 }
 
 impl<'a> Documentation<'a> {
-    pub fn new(state: &'a PointercrateState, page: &'a str) -> Result<Documentation<'a>> {
+    pub fn api_documentation(state: &'a PointercrateState, page: &'a str) -> Result<Documentation<'a>> {
         let content = match state.documentation_topics.get(page) {
             Some(cnt) => cnt,
             _ => return Err(PointercrateError::NotFound),
@@ -22,6 +24,23 @@ impl<'a> Documentation<'a> {
             toc: &*state.documentation_toc,
             content,
             page,
+            description: "The pointercrate API, which allows you to programmatically interface with the Demonlist",
+            title: "API Documentation",
+        })
+    }
+
+    pub fn guidelines(state: &'a PointercrateState, page: &'a str) -> Result<Documentation<'a>> {
+        let content = match state.guidelines_topics.get(page) {
+            Some(cnt) => cnt,
+            _ => return Err(PointercrateError::NotFound),
+        };
+
+        Ok(Documentation {
+            toc: &*state.guidelines_toc,
+            content,
+            page,
+            description: "The Demonlist guidelines regarding record submission/acceptance and level placements",
+            title: "Guidelines",
         })
     }
 }
@@ -31,23 +50,40 @@ impl<'a> Documentation<'a> {
 pub async fn index(state: PointercrateState) -> ViewResult<HttpResponse> {
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(Documentation::new(&state, "index")?.render().0))
+        .body(Documentation::api_documentation(&state, "index")?.render().0))
 }
 
 #[get("/documentation/{topic}/")]
 pub async fn topic(state: PointercrateState, topic: Path<String>) -> ViewResult<HttpResponse> {
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(Documentation::new(&state, &topic.into_inner())?.render().0))
+        .body(Documentation::api_documentation(&state, &topic.into_inner())?.render().0))
+}
+
+// actix complains if these aren't async, although they don't not have to be
+#[get("/guidelines/")]
+pub async fn guildelines_index(state: PointercrateState) -> ViewResult<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(Documentation::guidelines(&state, "index")?.render().0))
+}
+
+// cannot have multiple parameters with the same name in the same field it seems because actix_web
+// generates a unit struct for them.
+#[get("/guidelines/{gtopic}/")]
+pub async fn guidelines_topic(state: PointercrateState, gtopic: Path<String>) -> ViewResult<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(Documentation::guidelines(&state, &gtopic.into_inner())?.render().0))
 }
 
 impl<'a> Page for Documentation<'a> {
     fn title(&self) -> String {
-        format!("API Documentation - {}", self.page)
+        format!("{} - {}", self.title, self.page)
     }
 
     fn description(&self) -> String {
-        "The pointercrate API, which allows you to programmatically interface with the Demonlist".to_owned()
+        self.description.to_owned()
     }
 
     fn scripts(&self) -> Vec<&str> {
@@ -72,15 +108,10 @@ impl<'a> Page for Documentation<'a> {
                 <script>
                 // you know, this might be the most ugly solution to a problem I have ever thought of
                 $(document).ready(function() {
-                  $("h1").append(
-                    '<a class="fa fa-link fa-3 link-anchor" aria-hidden="true" title="Permanent link to this topic"></a>'
-                  );
-                  $("h1").prepend(
-                    '<i class="fa fa-link fa-3 link-anchor" style="visibility:hidden" aria-hidden="true"></i>'
-                  );
-                  $(".link-anchor").each((idx, elem) =>
-                    $(elem).attr("href", '#' + $(elem).parent()[0].id)
-                  );
+                  for(let header of document.getElementsByTagName("h1")) {
+                    header.innerHTML += '<a class="fa fa-link fa-3 link-anchor" aria-hidden="true" title="Permanent link to this topic" href = #' + (header.id || header.parentNode.id) + '></a>';
+                    header.innerHTML = '<i class="fa fa-link fa-3 link-anchor" style="visibility:hidden" aria-hidden="true"></i>' + header.innerHTML;
+                  }
                 })
                 </script>
             "#))
@@ -89,43 +120,43 @@ impl<'a> Page for Documentation<'a> {
 
     fn head(&self) -> Vec<Markup> {
         vec![html! {
-            (PreEscaped(r#"
+            (PreEscaped(format!(r#"
 <script type="application/ld+json">
-  {
+  {{
     "@context": "http://schema.org",
     "@type": "WebPage",
-    "breadcrumb": {
+    "breadcrumb": {{
       "@type": "BreadcrumbList",
-      "itemListElement": [{
+      "itemListElement": [{{
         "@type": "ListItem",
         "position": 1,
-        "item": {
+        "item": {{
           "@id": "https://pointercrate.com/",
           "name": "pointercrate"
-        }
-      },{
+        }}
+      }},{{
         "@type": "ListItem",
         "position": 2,
-        "item": {
+        "item": {{
           "@id": "https://pointercrate.com/documentation/",
           "name": "documentation"
-        }
-      },{
+        }}
+      }},{{
         "@type": "ListItem",
         "position": 3,
-        "item": {
+        "item": {{
             "@id": "https://pointercrate.com/documentation/account/",
             "name": "account"
-        }
-      }]
-    },
-    "name": "API Documentation",
-    "description": "The pointercrate API, which allows you to programmatically interface with the Demonlist",
+        }}
+      }}]
+    }},
+    "name": "{}",
+    "description": "{}",
     "url": "https://pointercrate.com/documentation/account/",
     "dateCreated": "2017-04-08"
-  }
+  }}
 </script>
-            "#))
+            "#, self.title, self.description)))
         }]
     }
 }
