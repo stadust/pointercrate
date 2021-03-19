cdd3fe7e559e96ad209f2e71412a73d8ba97f744
stadust <43299462+stadust@users.noreply.github.com>
2021-03-19 14:23:32 -0400

    insert ad below demon panel

--- src/view/demonlist/demon_page.rs
+++ src/view/demonlist/demon_page.rs
@@ -424,6 +424,21 @@ impl Page for Demonlist {
                     (super::submission_panel(&self.overview.demon_overview))
                     (super::stats_viewer(&self.overview.nations))
                     (self.demon_panel())
+                    div.panel.fade.js-scorll-anim.js-collapse data-anim = "fade" {
+                        (PreEscaped(r#"
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <!-- Demonpage sub demon panel ad -->
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-3064790497687357"
+                             data-ad-slot="6404050615"
+                             data-ad-format="auto"
+                             data-full-width-responsive="true"></ins>
+                        <script>
+                             (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                        "#))
+                    }
                     div.panel.fade.js-scroll-anim.js-collapse data-anim = "fade" {
                         h2.underlined.pad {
                             "Position History"
