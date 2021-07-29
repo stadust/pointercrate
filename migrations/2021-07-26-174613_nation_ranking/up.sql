ad4547e4cf400bc05ca460d5edc44c0a0c24063e
stadust <43299462+stadust@users.noreply.github.com>
2021-07-29 15:04:37 -0400

    change the sql query to produce a ranking according to what twitter wants instead

--- migrations/2021-07-26-174613_nation_ranking/up.sql
+++ migrations/2021-07-26-174613_nation_ranking/up.sql
@@ -4,54 +4,47 @@ CREATE OR REPLACE FUNCTION best_records_in(country VARCHAR(2))
     RETURNS TABLE (LIKE records)
     AS
 $body$
-    SELECT DISTINCT ON (demon) records.*
-    FROM records
-    INNER JOIN players
-    ON players.id = player
-    WHERE status_='APPROVED' AND players.nationality = country
-    ORDER BY demon, progress DESC;
+    WITH grp AS (
+        SELECT records.*,
+               RANK() OVER (PARTITION BY demon ORDER BY demon, progress DESC) AS rk
+        FROM records
+        INNER JOIN players
+        ON players.id = player
+        WHERE status_='APPROVED' AND players.nationality = country
+    )
+    SELECT id, progress, video, status_, player, submitter, demon
+    FROM grp
+    WHERE rk = 1;
 $body$
 LANGUAGE SQL;
 
 CREATE OR REPLACE VIEW nations_with_score AS
-    SELECT RANK() OVER(ORDER BY r.total_score DESC) AS rank,
-           r.total_score AS score,
+    SELECT RANK() OVER(ORDER BY scores.total_score DESC) AS rank,
+           scores.total_score AS score,
            nationalities.iso_country_code,
            nationalities.nation,
            nationalities.continent
     FROM (
-             SELECT nationality,
-                    SUM(scores.total_score) AS total_score
-             FROM (
-                      SELECT pseudo_records.player,
-                             SUM(record_score(pseudo_records.progress::FLOAT, pseudo_records.position::FLOAT,
-                                              100::FLOAT, pseudo_records.requirement)) as total_score
-                      FROM (
-                               SELECT player,
-                                      progress,
-                                      position,
-                                      CASE WHEN demons.position > 75 THEN 100 ELSE requirement END AS requirement
-                               FROM records
-                                        INNER JOIN demons
-                                                   ON demons.id = demon
-                               WHERE demons.position <= 150
-                                 AND status_ = 'APPROVED'
-
-                               UNION
-
-                               SELECT verifier                                                              as player,
-                                      CASE WHEN demons.position > 150 THEN 0.0::FLOAT ELSE 100.0::FLOAT END as progress,
-                                      position,
-                                      100.0::FLOAT
-                               FROM demons
-                           ) AS pseudo_records
-                      GROUP BY player
-                  ) scores
-                      INNER JOIN players
-                                 ON scores.player = players.id
-             WHERE NOT players.banned
-               AND nationality IS NOT NULL
-             GROUP BY nationality
-         ) r
+          SELECT nationality,
+                 SUM(record_score(pseudo_records.progress::FLOAT, pseudo_records.position::FLOAT,
+                                  100::FLOAT, pseudo_records.requirement)) as total_score
+          FROM (
+                   select distinct on (nationality, demon)
+                       nationality,
+                       progress,
+                       position,
+                       CASE WHEN demons.position > 75 THEN 100 ELSE requirement END AS requirement
+                   from records
+                       inner join demons
+                           on demons.id = records.demon
+                       inner join players
+                           on players.id=records.player
+                       inner join nationalities
+                           on iso_country_code=players.nationality
+                   where status_='APPROVED' and position <= 150 and not players.banned
+                   order by nationality, demon, progress desc
+               ) AS pseudo_records
+          GROUP BY nationality
+   ) scores
 INNER JOIN nationalities
-        ON nationalities.iso_country_code = r.nationality;
+        ON nationalities.iso_country_code = scores.nationality;
