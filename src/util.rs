00983b159b71f5eb38caab67c615451a839b4350
stadust <43299462+stadust@users.noreply.github.com>
2021-08-14 09:03:13 -0400

    remove submitter stuff from main crate

--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,6 @@ use crate::error::PointercrateError;
 use actix_web::http::HeaderMap;
 use log::warn;
 use mime::Mime;
-use serde::{de::Error, Deserialize, Deserializer};
 use std::str::FromStr;
 
 macro_rules! pagination_response {
@@ -184,22 +183,4 @@ macro_rules! header {
     };
 }*/
 
-#[allow(clippy::option_option)]
-pub fn nullable<'de, T, D>(deserializer: D) -> std::result::Result<Option<Option<T>>, D::Error>
-where
-    D: Deserializer<'de>,
-    T: Deserialize<'de>,
-{
-    Ok(Some(Option::deserialize(deserializer)?))
-}
-
-pub fn non_nullable<'de, T, D>(deseralizer: D) -> std::result::Result<Option<T>, D::Error>
-where
-    D: Deserializer<'de>,
-    T: Deserialize<'de>,
-{
-    match Option::deserialize(deseralizer)? {
-        None => Err(<D as Deserializer<'de>>::Error::custom("null value on non-nullable field")),
-        some => Ok(some),
-    }
-}
+pub use pointercrate_core::util::*;
