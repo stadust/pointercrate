29811a152e0b8edbed9bcf082d885f8b19e10b89
stadust <43299462+stadust@users.noreply.github.com>
2021-08-30 05:44:56 -0400

    frontend for player claims and fix of claim pagination causing a 500

--- pointercrate-demonlist-pages/src/account/list_integration.rs
+++ pointercrate-demonlist-pages/src/account/list_integration.rs
@@ -1,8 +1,8 @@
 use maud::{html, Markup, PreEscaped};
 use pointercrate_core::{error::PointercrateError, permission::PermissionsManager};
-use pointercrate_core_pages::{error::ErrorFragment, PageFragment, Script};
+use pointercrate_core_pages::{error::ErrorFragment, util::paginator, PageFragment, Script};
 use pointercrate_demonlist::player::claim::PlayerClaim;
-use pointercrate_user::{sqlx::PgConnection, User};
+use pointercrate_user::{sqlx::PgConnection, User, MODERATOR};
 use pointercrate_user_pages::account::AccountPageTab;
 
 pub struct ListIntegrationTab;
@@ -14,7 +14,7 @@ impl AccountPageTab for ListIntegrationTab {
     }
 
     fn additional_scripts(&self) -> Vec<Script> {
-        vec![]
+        vec![Script::module("/static/js/account/integration.js")]
     }
 
     fn tab_id(&self) -> u8 {
@@ -45,44 +45,75 @@ impl AccountPageTab for ListIntegrationTab {
 
         html! {
             div.left {
-                div.panel.fade.flex.no-stretch style="padding: 5px; text-align: left; justify-content: space-between" {
-                    @match player_claim {
-                        Some(claim) => {
-                            div {
-                                b {
-                                    "Claim on player: "
-                                }
-                                i {
-                                    (claim.player_id)
-                                }
-                                br;
-                                b {
-                                    "Status: "
-                                }
-                                i {
-                                    @if claim.verified {
-                                        "verified"
+                div.panel.fade.js-collapse style="text-align: left; padding: 10px 20px" {
+                    div.flex.no-stretch style="justify-content: space-between; align-items: center; " {
+                        span style = "font-size: 1.3em" {
+                            i.fa.fa-pencil-alt.clickable#player-claim-pen aria-hidden = "true" {} (PreEscaped("&nbsp;"))
+                            b {
+                                "Claimed Player: "
+                            }
+                            @match player_claim {
+                                Some(ref claim) => {
+                                    i#claimed-player {
+                                        (claim.player_id)
                                     }
-                                    @else {
-                                        "unverified"
+                                },
+                                None => {
+                                    i {
+                                        "None"
                                     }
                                 }
                             }
-                            a.button.blue.hover {
-                                "Change claim"
+                        }
+                        span {
+                            @match player_claim {
+                                Some(ref claim) if claim.verified => {
+                                    i style="margin-right: 15px;" {
+                                        "Verified"
+                                    }
+                                    span.arrow.hover {}
+                                },
+                                _ => i{"Unverified"}
                             }
-                        },
-                        None => {
-                            i {
-                                "No player currently claimed"
-                            }
-                            a.button.blue.hover {
-                                "Initiate claim"
+                        }
+                    }
+                    @if let Some(ref claim) = player_claim {
+                        @if claim.verified {
+                            div.overlined.pad.js-collapse-content style="display:none" {
+                                div.flex.no-stretch style="justify-content: space-between; align-items: center" {
+                                    b {
+                                        "Geolocate nationality:"
+                                    }
+                                    a.button.blue.hover {
+                                        "Go"
+                                    }
+                                }
+                                p {
+                                    "Clicking the above button let's you set your claimed player's nationality via IP Geolocation. To offer this functionality, pointercrate uses "
+                                    a.link href = "https://www.abstractapi.com/ip-geolocation-api" { "abstract's IP geolocation API"}
+                                    ". Clicking the above button also counts as your consent for pointercrate to send your IP to abstract."
+                                }
                             }
                         }
                     }
                 }
-                div style="height: 50px" {} // to make sure that the footer doesnt float. if it floats, the user page is the only one without a scrollbar at the right, which causes jumpyness when switching tabs.
+                @if permissions.require_permission(user.permissions, MODERATOR).is_ok() {
+                    div.panel.fade {
+                        h2.pad.underlined {
+                            "Manage Claims"
+                        }
+                        p {
+                            "Manage claims using the interface below. The list can be filtered by player and user using the panels on the right. Invalid claims should be deleted using the trash icon. "
+                            br;
+                            "To verify a claim, click the checkmark. Only verify claims you have verified to be correct (this will probably mean talking to the player that's being claimed, and asking if they initiated the claim themselves, or if the claim is malicious."
+                            br;
+                            "Once a claim on a player is verified, all other unverified claims on that player are auto-deleted. Users cannot put new, unverified claims on players that have a verified claim on them."
+                            br;
+                            "A claim with a green background is verified, a claim with a blue background is unverified/unchecked"
+                        }
+                        (paginator("claim-pagination", "/api/v1/players/claims/"))
+                    }
+                }
             }
             div.right {
                 div.panel.fade {
--- pointercrate-demonlist/sql/paginate_claims.sql
+++ pointercrate-demonlist/sql/paginate_claims.sql
@@ -1,6 +1,6 @@
-SELECT player_claims.id, members.member_id AS mid, members.name AS mname, players.id AS pid, players.name as pname, verified
+SELECT player_claims.id, members.member_id AS mid, members.name AS mname, players.id AS pid, players.name::TEXT as pname, verified
 FROM player_claims
-    INNER JOIN members on member_id=user_id
+    INNER JOIN members on members.member_id=player_claims.member_id
     INNER JOIN players on players.id=player_id
 WHERE (player_claims.id < $1 OR $1 IS NULL)
   AND (player_claims.id > $2 OR $2 IS NULL)
