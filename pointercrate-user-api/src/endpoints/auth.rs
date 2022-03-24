11e8de5d50453dc06e0891a7db1d9d9a738c47c7
stadust <43299462+stadust@users.noreply.github.com>
2022-03-24 10:13:49 +0000

    actually make verification endpoint work by commiting the transaction

--- pointercrate-user-api/src/endpoints/auth.rs
+++ pointercrate-user-api/src/endpoints/auth.rs
@@ -65,7 +65,9 @@ pub async fn invalidate(mut auth: BasicAuth) -> Result<Status> {
 #[rocket::get("/verify_email?<token>")]
 pub async fn verify_email(mut auth: TokenAuth, token: &str) -> Result<&'static str> {
     let email = auth.user.validate_change_email_token(token)?;
+
     auth.user.set_email_address(email, &mut auth.connection).await?;
+    auth.connection.commit().await.map_err(UserError::from)?;
 
     Ok("Success! You can close this tab/window now")
 }
