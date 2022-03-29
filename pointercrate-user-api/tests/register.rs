c749383988c6eff994796436cd4fcf5ad40007bd
stadust <43299462+stadust@users.noreply.github.com>
2022-03-29 12:31:02 +0100

    one more proof of concept integration test

--- pointercrate-user-api/tests/register.rs
+++ pointercrate-user-api/tests/register.rs
@@ -6,7 +6,7 @@ mod setup;
 #[rocket::async_test]
 #[serial_test::serial]
 pub async fn register_new() {
-    let client = setup::setup().await;
+    let (client, _) = setup::setup().await;
 
     let response = client
         .post("/api/v1/auth/register/")
@@ -20,4 +20,23 @@ pub async fn register_new() {
         .await;
 
     assert_eq!(response.status(), Status::Created)
+}
+
+#[rocket::async_test]
+#[serial_test::serial]
+pub async fn register_taken_username() {
+    let (client, ..) = setup::setup_with_admin_user().await;
+
+    let response = client
+        .post("/api/v1/auth/register/")
+        .header(ContentType::JSON)
+        .json(&Registration {
+            name: "Patrick".to_string(),
+            password: "bad password".to_string(),
+        })
+        .header(Header::new("X-Real-IP", "127.0.0.1"))
+        .dispatch()
+        .await;
+
+    assert_eq!(response.status(), Status::Conflict)
 }
--- pointercrate-user-api/tests/setup.rs
+++ pointercrate-user-api/tests/setup.rs
@@ -1,10 +1,12 @@
 use pointercrate_core::{permission::PermissionsManager, pool::PointercratePool};
-use pointercrate_user::{ADMINISTRATOR, MODERATOR};
+use pointercrate_user::{AuthenticatedUser, Registration, User, ADMINISTRATOR, MODERATOR};
 use pointercrate_user_pages::account::AccountPageConfig;
 use rocket::{local::asynchronous::Client, Build, Rocket};
+use sqlx::{pool::PoolConnection, Pool, Postgres};
 
-pub async fn setup() -> Client {
+pub async fn setup() -> (Client, PoolConnection<Postgres>) {
     let pool = PointercratePool::init().await;
+    let connection = pool.connection().await.unwrap();
 
     // reset test database
     sqlx::query!("TRUNCATE TABLE members CASCADE")
@@ -21,5 +23,30 @@ pub async fn setup() -> Client {
         .manage(permissions)
         .manage(AccountPageConfig::default());
 
-    Client::tracked(rocket).await.unwrap()
+    (Client::tracked(rocket).await.unwrap(), connection)
+}
+
+pub async fn setup_with_admin_user() -> (Client, AuthenticatedUser, PoolConnection<Postgres>) {
+    let (client, mut connection) = setup().await;
+
+    let user = AuthenticatedUser::register(
+        Registration {
+            name: "Patrick".to_string(),
+            password: "bad password".to_string(),
+        },
+        &mut connection,
+    )
+    .await
+    .unwrap();
+
+    sqlx::query!(
+        "UPDATE members SET permissions = $2::INTEGER::BIT(16) WHERE member_id = $1",
+        user.inner().id,
+        ADMINISTRATOR.bit() as i16
+    )
+    .execute(&mut connection)
+    .await
+    .unwrap();
+
+    (client, user, connection)
 }
