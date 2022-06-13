f44f123a1aa9afbe214bccb041d2574099758385
stadust <43299462+stadust@users.noreply.github.com>
2022-06-13 19:13:47 +0100

    extended buff as in #59

--- pointercrate-demonlist/src/demon/mod.rs
+++ pointercrate-demonlist/src/demon/mod.rs
@@ -229,16 +229,17 @@ impl Demon {
     pub fn score(&self, progress: i16) -> f64 {
         let position = self.base.position;
 
-        let beaten_score = if 125 < position && position <= 150 {
-            150f64 * f64::exp((1f64 - f64::from(position)) * (1f64 / 30f64).ln() / (-149f64))
-        } else if 50 < position && position <= 125 {
-            let a = 2.333f64;
-            let b = 1.884f64;
-            60f64 * (a.powf((51 - position) as f64 * ((30f64.ln()) / 99f64))) + b
-        } else if 20 < position && position <= 50 {
-            let c = 1.01327f64;
-            let d = 26.489f64;
-            -100f64 * (c.powf(position as f64 - d)) + 200f64
+        let beaten_score = if 55 < position && position <= 150 {
+            let b = 6.273f64;
+            56.191f64 * (2f64.powf((54.147f64 - (position as f64 + 3.2f64) as f64) * ((50f64.ln()) / 99f64))) + b
+        } else if 35 < position && position <= 55 {
+            let g = 1.036f64;
+            let h = 25.071f64;
+            212.61f64 * (g.powf(1f64 - position as f64)) + h
+        } else if 20 < position && position <= 35 {
+            let c = 1.0099685f64;
+            let d = 31.152f64;
+            (250f64 - 83.389f64) * (c.powf(2f64 - position as f64)) - d
         } else if 0 < position && position <= 20 {
             let e = 1.168f64;
             let f = 100.39f64;
