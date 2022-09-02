600be0e131c5c3e4214045b62d50f10b7efc5ae6
NimbusGD <71958627+NimbusGD@users.noreply.github.com>
2022-09-01 23:51:05 -0500

    Add is_raw_footage option

--- pointercrate-demonlist/src/record/note/mod.rs
+++ pointercrate-demonlist/src/record/note/mod.rs
@@ -21,6 +21,8 @@ pub struct Note {
     pub content: String,
 
     pub is_public: bool,
+    
+    pub is_raw_footage: bool,
 
     /// Whether this note was originally made on a different record and later transferred to this
     /// one due to deletion.
