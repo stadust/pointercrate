e5c7c5b3a79312aac282a841caed5dc52c878d5c
stadust <43299462+stadust@users.noreply.github.com>
2023-10-21 12:59:31 +0100

    Teach frontend about Cloudflare ratelimits
    Currently, if the frontend code encounters a cloudflare ratelimit (a 429
    response with an HTML body), it will misinterpret this as a nginx 502
    error and print "severe internal server error". Needless to say, this is
    confusing and wrong. Now it will correctly display a message about a
    cloudflare ratelimit.

--- pointercrate-core-pages/static/js/modules/form.js
+++ pointercrate-core-pages/static/js/modules/form.js
@@ -1184,6 +1184,11 @@ const UNEXPECTED_REDIRECT = {
   code: 50000,
   data: null,
 };
+const RATELIMITED = {
+  message: "You have hit a Cloudflare ratelimit. Please wait a short time and try again.",
+  code: 42900,
+  data: null
+}
 
 function mkReq(method, endpoint, headers = {}, data = null) {
   headers["Content-Type"] = "application/json";
@@ -1219,7 +1224,7 @@ function mkReq(method, endpoint, headers = {}, data = null) {
           var jsonError = JSON.parse(xhr.responseText);
         } catch (e) {
           return reject({
-            data: SEVERE_ERROR,
+            data: xhr.status == 429 ? RATELIMITED : SEVERE_ERROR,
             headers: parseHeaders(xhr),
             status: xhr.status,
           });
