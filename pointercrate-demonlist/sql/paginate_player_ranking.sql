stadust/pointercrate

commit aee50b673d5ef1105703946598c789b8c8aab77b

fix: Make stats viewer return deterministic ranking for ties
In the case of ties on the stats viewer, postgres' `RANK` function
returns the same rank for multiple rows. This means that by only doing
`ORDER BY rank`, we essentially order these rows non-deterministically.
This is bad for pagination, as if a page breaks between two players with
the same score, different orderings chosen by postgres between the two
requests could result in some players occuring multiple times, while
others will be missing altogether.

pointercrate-demonlist/sql/paginate_player_ranking.sql
@@ -6,5 +6,5 @@ WHERE (index < $1 OR $1 IS NULL)
   AND (nation = $4 OR iso_country_code = $4 OR (nation IS NULL AND $5) OR ($4 IS NULL AND NOT $5))
   AND (continent = CAST($6::TEXT AS continent) OR $6 IS NULL)
   AND (subdivision = $7 OR $7 IS NULL)
-ORDER BY rank {}
+ORDER BY rank, id {}
 LIMIT $8

commit 043e8a967969fb59915d98a361c8890ab0ef5ec9

fix: passing only ?before= to /ranking gave top 50 in reverse
When adding the secondary `ORDER BY` expression to the player ranking
query, the sorting (`DESC` or `ASC`) was only applied to the `id`
column, not the `rank` column. However, due to how pagination works with
`?before` parameters (which queries the database by grabbing the first
`limit` items in reverse and then reversing the result again in the
pagination code), this caused pagination for the stats viewer to break.

Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>
Fixes: aee50b673d5ef1105703946598c789b8c8aab77b

pointercrate-demonlist/sql/paginate_player_ranking.sql
@@ -6,5 +6,5 @@ WHERE (index < $1 OR $1 IS NULL)
   AND (nation = $4 OR iso_country_code = $4 OR (nation IS NULL AND $5) OR ($4 IS NULL AND NOT $5))
   AND (continent = CAST($6::TEXT AS continent) OR $6 IS NULL)
   AND (subdivision = $7 OR $7 IS NULL)
-ORDER BY rank, id {}
+ORDER BY rank {}, id
 LIMIT $8
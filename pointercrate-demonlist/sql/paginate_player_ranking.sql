043e8a967969fb59915d98a361c8890ab0ef5ec9
stadust <43299462+stadust@users.noreply.github.com>
2024-03-28 13:43:45 +0000

    fix: passing only ?before= to /ranking gave top 50 in reverse
    When adding the secondary `ORDER BY` expression to the player ranking
    query, the sorting (`DESC` or `ASC`) was only applied to the `id`
    column, not the `rank` column. However, due to how pagination works with
    `?before` parameters (which queries the database by grabbing the first
    `limit` items in reverse and then reversing the result again in the
    pagination code), this caused pagination for the stats viewer to break.
    
    Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>
    Fixes: aee50b673d5ef1105703946598c789b8c8aab77b

--- pointercrate-demonlist/sql/paginate_player_ranking.sql
+++ pointercrate-demonlist/sql/paginate_player_ranking.sql
@@ -6,5 +6,5 @@ WHERE (index < $1 OR $1 IS NULL)
   AND (nation = $4 OR iso_country_code = $4 OR (nation IS NULL AND $5) OR ($4 IS NULL AND NOT $5))
   AND (continent = CAST($6::TEXT AS continent) OR $6 IS NULL)
   AND (subdivision = $7 OR $7 IS NULL)
-ORDER BY rank, id {}
+ORDER BY rank {}, id
 LIMIT $8
