stadust/pointercrate

commit fa821eeb62b8146387519e4adf7a53f28c313280

Add option to hide time machine destination panel
Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

pointercrate-demonlist-pages/src/components/time_machine.rs
@@ -7,30 +7,35 @@ pub enum Tardis {
     Activated {
         destination: DateTime<FixedOffset>,
         demons: Vec<TimeShiftedDemon>,
-        visible: bool,
+        /// Whether the time selection panel should be visible.
+        show_selector: bool,
+        /// Whether the "You are currently looking at the demonlist as it was on ..." panel should be visible.
+        show_destination: bool
     },
     Deactivated {
-        visible: bool,
+        /// Whether the time selection panel should be visible.
+        show_selector: bool,
     },
 }
 
 impl Tardis {
     pub fn new(visible: bool) -> Self {
-        Tardis::Deactivated { visible }
+        Tardis::Deactivated { show_selector: visible }
     }
 
-    pub fn activate(self, destination: DateTime<FixedOffset>, demons_then: Vec<TimeShiftedDemon>) -> Self {
-        Tardis::Activated {
-            visible: self.visible(),
+    pub fn activate(&mut self, destination: DateTime<FixedOffset>, demons_then: Vec<TimeShiftedDemon>, show_destination: bool) {
+        *self = Tardis::Activated {
+            show_selector: self.visible(),
             demons: demons_then,
             destination,
-        }
+            show_destination
+        };
     }
 
     pub fn visible(&self) -> bool {
         match self {
-            Tardis::Activated { visible, .. } => *visible,
-            Tardis::Deactivated { visible } => *visible,
+            Tardis::Activated { show_selector: visible, .. } => *visible,
+            Tardis::Deactivated { show_selector: visible } => *visible,
         }
     }
 }

pointercrate-demonlist-api/src/pages.rs
@@ -48,7 +48,8 @@ pub async fn overview(
 
     // On april's fools, ignore the cookie and just pick a random day to display
     let today = Utc::now().naive_utc();
-    if today.day() == 1 && today.month() == 4 {
+    let is_april_1st = today.day() == 1 && today.month() == 4;
+    if is_april_1st {
         let seconds_since_beginning_of_time = (today - beginning_of_time).num_seconds();
         let go_back_by = chrono::Duration::seconds(rand::thread_rng().gen_range(0..seconds_since_beginning_of_time));
 
@@ -66,12 +67,12 @@ pub async fn overview(
         _ => None,
     };
 
-    let tardis = match specified_when {
-        Some(destination) => {
-            Tardis::new(timemachine.unwrap_or(false)).activate(destination, list_at(&mut *connection, destination.naive_utc()).await?)
-        },
-        _ => Tardis::new(timemachine.unwrap_or(false)),
-    };
+    let mut tardis = Tardis::new(timemachine.unwrap_or(false));
+
+    if let Some(destination) = specified_when {
+        let demons_then = list_at(&mut *connection, destination.naive_utc()).await?;
+        tardis.activate(destination, demons_then, !is_april_1st)
+    }
 
     let mut page = Page::new(OverviewPage {
         team: Team {

commit dff3832c879862736940245fd6f047d812b440b0

fix: Actually hide time machine destintion panel if !show_destination
Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>
Fixes: fa821eeb62b8146387519e4adf7a53f28c313280

pointercrate-demonlist-pages/src/components/time_machine.rs
@@ -48,7 +48,7 @@ impl Render for Tardis {
     fn render(&self) -> Markup {
         html! {
             @match self {
-                Tardis::Activated { destination, ..} => {
+                Tardis::Activated { destination, show_destination, ..} if *show_destination => {
                     div.panel.fade.blue.flex style="align-items: center;" {
                         span style = "text-align: end"{
                             "You are currently looking at the demonlist how it was on"