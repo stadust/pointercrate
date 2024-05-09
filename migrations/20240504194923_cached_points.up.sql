3738f8a8a77b6b0056e235e4ef4ec7524c37e60f
stadust <43299462+stadust@users.noreply.github.com>
2024-05-09 21:25:04 +0100

    Drop some now-unused SQL functions
    Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

--- migrations/20240504194923_cached_points.up.sql
+++ migrations/20240504194923_cached_points.up.sql
@@ -136,3 +136,7 @@ CREATE VIEW ranked_nations AS
         continent
     FROM nationalities
     WHERE score > 0.0;
+
+-- Now-unused functions
+DROP FUNCTION subdivision_ranking_of(country varchar(2));
+DROP FUNCTION best_records_local(country VARCHAR(2), the_subdivision VARCHAR(3));
