02ae94afd5d8d1ebcd9d2a31870d36ed08bf05e7
stadust <43299462+stadust@users.noreply.github.com>
2024-07-31 19:19:51 +0100

    test: convert thumbnail related integration tests to unittests
    I don't know why I have absolutely refused to write unit tests in the
    past. For a lot of functionality, integration tests are needed, as the
    code under test either lives inside a request handler, or spans
    interactions between multiple crates (player claims for example). But
    for test cases that only concern themselves with for example a single
    endpoint, and then only test functionality that lives in, for example,
    `pointercrate-demonlist`, we can just use unit tests.
    
    Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

--- pointercrate-demonlist/src/demon/post.rs
+++ pointercrate-demonlist/src/demon/post.rs
@@ -83,3 +83,77 @@ impl FullDemon {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use sqlx::{pool::PoolConnection, Postgres};
+
+    use crate::demon::{FullDemon, PostDemon};
+
+    const DEFAULT_THUMBNAIL: &str = "https://i.ytimg.com/vi/zebrafishes/mqdefault.jpg";
+
+    #[sqlx::test(migrations = "../migrations")]
+    async fn test_default_thumbnail_no_video(mut conn: PoolConnection<Postgres>) {
+        let demon = FullDemon::create_from(
+            PostDemon {
+                name: "Bloodbath".to_owned(),
+                position: 1,
+                requirement: 90,
+                verifier: "Riot".to_owned(),
+                publisher: "Riot".to_owned(),
+                creators: Vec::new(),
+                video: None,
+            },
+            &mut conn,
+        )
+        .await
+        .unwrap();
+
+        assert_eq!(demon.demon.thumbnail, DEFAULT_THUMBNAIL);
+    }
+
+    #[sqlx::test(migrations = "../migrations")]
+    async fn test_default_thumbnail_linked_banned(mut conn: PoolConnection<Postgres>) {
+        sqlx::query!("INSERT INTO players (name, link_banned) VALUES ('Riot', TRUE)")
+            .execute(&mut *conn)
+            .await
+            .unwrap();
+
+        let demon = FullDemon::create_from(
+            PostDemon {
+                name: "Bloodbath".to_owned(),
+                position: 1,
+                requirement: 90,
+                verifier: "Riot".to_owned(),
+                publisher: "Riot".to_owned(),
+                creators: Vec::new(),
+                video: Some("https://www.youtube.com/watch?v=dQw4w9WgXcQ".to_owned()),
+            },
+            &mut conn,
+        )
+        .await
+        .unwrap();
+
+        assert_eq!(demon.demon.thumbnail, DEFAULT_THUMBNAIL);
+    }
+
+    #[sqlx::test(migrations = "../migrations")]
+    async fn test_default_thumbnail_with_video(mut conn: PoolConnection<Postgres>) {
+        let demon = FullDemon::create_from(
+            PostDemon {
+                name: "Bloodbath".to_owned(),
+                position: 1,
+                requirement: 90,
+                verifier: "Riot".to_owned(),
+                publisher: "Riot".to_owned(),
+                creators: Vec::new(),
+                video: Some("https://www.youtube.com/watch?v=dQw4w9WgXcQ".to_owned()),
+            },
+            &mut conn,
+        )
+        .await
+        .unwrap();
+
+        assert_eq!(demon.demon.thumbnail, "https://i.ytimg.com/vi/dQw4w9WgXcQ/mqdefault.jpg");
+    }
+}
--- pointercrate-test/tests/demonlist/demon.rs
+++ pointercrate-test/tests/demonlist/demon.rs
@@ -8,8 +8,6 @@ use pointercrate_demonlist::{
 use rocket::http::Status;
 use sqlx::{Pool, Postgres};
 
-const DEFAULT_THUMBNAIL: &str = "https://i.ytimg.com/vi/zebrafishes/mqdefault.jpg";
-
 #[sqlx::test(migrations = "../migrations")]
 async fn test_add_demon_ratelimits(pool: Pool<Postgres>) {
     let (clnt, mut connection) = pointercrate_test::demonlist::setup_rocket(pool).await;
@@ -34,77 +32,6 @@ async fn test_add_demon_ratelimits(pool: Pool<Postgres>) {
         .await;
 
     assert_eq!(result["code"].as_i64(), Some(42900))
-}
-
-#[sqlx::test(migrations = "../migrations")]
-async fn test_default_thumbnail_no_video(pool: Pool<Postgres>) {
-    let (clnt, mut connection) = pointercrate_test::demonlist::setup_rocket(pool).await;
-
-    let user = pointercrate_test::user::system_user_with_perms(LIST_MODERATOR, &mut *connection).await;
-
-    let demon = serde_json::json! {{"name": "Bloodbath", "requirement": 90, "position": 1, "verifier": "Riot", "publisher": "Riot", "creators": []}};
-
-    // first one should succeed
-    let result: serde_json::Value = clnt
-        .post("/api/v2/demons/", &demon)
-        .authorize_as(&user)
-        .expect_status(Status::Created)
-        .get_result()
-        .await;
-
-    dbg!(&result);
-
-    assert_eq!(result["data"]["thumbnail"].as_str(), Some(DEFAULT_THUMBNAIL))
-}
-
-#[sqlx::test(migrations = "../migrations")]
-async fn test_default_thumbnail_linked_banned(pool: Pool<Postgres>) {
-    let (clnt, mut connection) = pointercrate_test::demonlist::setup_rocket(pool).await;
-
-    let user = pointercrate_test::user::system_user_with_perms(LIST_MODERATOR, &mut *connection).await;
-
-    let demon = serde_json::json! {{"name": "Bloodbath", "requirement": 90, "position": 1, "verifier": "Riot", "publisher": "Riot", "creators": [], "video": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}};
-
-    sqlx::query!("INSERT INTO players (name, link_banned) VALUES ('Riot', TRUE)")
-        .execute(&mut *connection)
-        .await
-        .unwrap();
-
-    // first one should succeed
-    let result: serde_json::Value = clnt
-        .post("/api/v2/demons/", &demon)
-        .authorize_as(&user)
-        .expect_status(Status::Created)
-        .get_result()
-        .await;
-
-    dbg!(&result);
-
-    assert_eq!(result["data"]["thumbnail"].as_str(), Some(DEFAULT_THUMBNAIL))
-}
-
-#[sqlx::test(migrations = "../migrations")]
-async fn test_default_thumbnail_with_video(pool: Pool<Postgres>) {
-    let (clnt, mut connection) = pointercrate_test::demonlist::setup_rocket(pool).await;
-
-    let user = pointercrate_test::user::system_user_with_perms(LIST_MODERATOR, &mut *connection).await;
-
-    let demon = serde_json::json! {{"name": "Bloodbath", "requirement": 90, "position": 1, "verifier": "Riot", "publisher": "Riot", "creators": [], "video": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}};
-
-    // first one should succeed
-    let result: serde_json::Value = clnt
-        .post("/api/v2/demons/", &demon)
-        .authorize_as(&user)
-        .expect_status(Status::Created)
-        .get_result()
-        .await;
-
-    dbg!(&result);
-
-    assert_eq!(
-        result["data"]["thumbnail"].as_str(),
-        Some("https://i.ytimg.com/vi/dQw4w9WgXcQ/mqdefault.jpg")
-    )
 }
 
 #[sqlx::test(migrations = "../migrations")]
