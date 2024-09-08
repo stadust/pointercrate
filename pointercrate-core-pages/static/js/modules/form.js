stadust/pointercrate

commit aa1f0a7c4310e2261c29b1aa1029f8f550fe2b29

fix(js): drop `parseInt` from `DropDownFormInput.value`
Originally, these were written for the time machine, as the time
machine's input used to be a set of 6 drop down menus with numbers (for
day, hour, month, etc). Later they also became used for booleans and
nationalities, which is where the "if parses as number, return number,
else return string" logic came from. This was fine, as nations and
booleans could never falsely parse as number. Then the time machine got
updated to use proper datetime inputs
(d7cd23e73b54fa5a07297f184248a109440c5e44), and the integer parsing
became unneeded (but at this point, I had forgotten it even existed).

Now fast forward (~to 300 take out coffees later~) to today, where the same
dropdown is used for player selection. But players could have names
start with numbers (or just be numbers altogether). Since JavaScript's
designers made the excellent decision that `parseInt("12aksldfj")`
should return `12`, this causes all sorts of breakage. Luckily, since
the time machine uses a proper date picker these days, we can just drop
the `parseInt` hack.

Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

pointercrate-core-pages/static/js/modules/form.js
@@ -957,9 +957,7 @@ export class DropdownFormInput extends FormInput {
   }
 
   get value() {
-    // FIXME: obviously not always int
-    let asInt = parseInt(this.dropdown.selected);
-    return isNaN(asInt) ? this.dropdown.selected : asInt;
+    return this.dropdown.selected;
   }
 
   get name() {

commit d61e79e6db53c0e4d21e10f1f2c86d0643724759

fix(js): Do not submit demon id as string in record submitter
Turns out the parseInt hack was needed somewhere else after. Fix this
properly by introducing "value transformers" for form inputs.

Fixes: aa1f0a7c4310e2261c29b1aa1029f8f550fe2b29
Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

pointercrate-core-pages/static/js/modules/form.js
@@ -752,6 +752,11 @@ export class FormInput {
   constructor() {
     this._clearOnInvalid = false;
     this._validators = [];
+    this._transform = (x) => x;
+  }
+
+  setTransform(transform) {
+    this._transform = transform;
   }
 
   addValidator(validator, msg) {
@@ -809,6 +814,10 @@ export class FormInput {
    */
   get value() {
     throw new Error("Abstract Property");
+  }
+
+  get transformedValue() {
+    return this._transform(this.value);
   }
 
   set value(value) {
@@ -1107,7 +1116,7 @@ export class Form extends Output {
 
     for (let input of this.inputs) {
       if (input.name && (input.value !== null || !input.required)) {
-        data[input.name] = input.value;
+        data[input.name] = input.transformedValue;
       }
     }
 

pointercrate-demonlist-pages/static/js/modules/demonlist.js
@@ -78,6 +78,7 @@ export function initializeRecordSubmitter(submitApproved = false) {
   var rawFootage = submissionForm.input("submit-raw-footage");
 
   demon.addValidator(input => input.dropdown.selected !== undefined, "Please specify a demon");
+  demon.setTransform(parseInt);
 
   player.addValidator(input => input.value !== undefined, "Please specify a record holder");
   player.addValidator(