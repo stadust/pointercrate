065427b631ec748b4a471433524f3dfb2d7d7557
sphericle <jackb2033@gmail.com>
2024-09-11 19:55:30 -0400

    test

---
+++ pointercrate-example/sample/migrations/comments/20190312131253_nationality.down.sql
@@ -0,0 +1,7 @@
+/* -- This file should undo anything in `up.sql`
+
+ALTER TABLE players DROP COLUMN nationality;
+ALTER TABLE members DROP COLUMN nationality;
+
+DROP TABLE nationalities;
+ */
---
+++ pointercrate-example/sample/migrations/comments/20240907133101_resync_database.up.sql
@@ -0,0 +1,9 @@
+/* ALTER TABLE IF EXISTS public.demons
+    DROP CONSTRAINT IF EXISTS demons_level_id_fkey;
+
+
+
+
+ALTER TABLE IF EXISTS public.records
+    DROP CONSTRAINT IF EXISTS records_video_key;
+ */
