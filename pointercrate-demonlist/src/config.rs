20310936cb829fb7e7cd22ce9b8ab3276f6807b8
sphericle <86196223+sphericle@users.noreply.github.com>
2024-09-12 14:34:37 -0400

    Update config.rs

--- pointercrate-demonlist/src/config.rs
+++ pointercrate-demonlist/src/config.rs
@@ -1,9 +1,9 @@
 use pointercrate_core::util::from_env_or_default;
 
 pub fn list_size() -> i16 {
-    from_env_or_default("LIST_SIZE", 75)
+    from_env_or_default("LIST_SIZE", 100)
 }
 
 pub fn extended_list_size() -> i16 {
-    from_env_or_default("EXTENDED_LIST_SIZE", 100)
+    from_env_or_default("EXTENDED_LIST_SIZE", 200)
 }
