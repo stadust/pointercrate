db3fb22ce644f8b1b6b1ae476660078fcd41bb82
sphericle <86196223+sphericle@users.noreply.github.com>
2024-09-12 14:56:46 -0400

    refac: don’t use env for list sizes
    because the deploy script OVERWRITES the env file instead of appending the db url to it, these functions will always use the default. this is fineC so we can just stop checking the env altogether

--- pointercrate-demonlist/src/config.rs
+++ pointercrate-demonlist/src/config.rs
@@ -1,9 +1,3 @@
-use pointercrate_core::util::from_env_or_default;
+pub fn list_size() -> i16 { 100 }
 
-pub fn list_size() -> i16 {
-    from_env_or_default("LIST_SIZE", 100)
-}
-
-pub fn extended_list_size() -> i16 {
-    from_env_or_default("EXTENDED_LIST_SIZE", 200)
-}
+pub fn extended_list_size() -> i16 { 200 }
