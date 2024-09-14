stadust/pointercrate

commit 15d808d345a16343aafb8a310aee7bb499c1a408

More refactors to prepare for oauth login (#172)
* refactor: Move legacy-account related functionality into module

Takes the refactor of #171 a few steps further. Instead of only turning
`AuthenticationMethod` into an enum, turn `AuthenticatedUser` into an
enum. This enforces at the type level that things like
`invalidate_all_tokens` has an implementation specific to the
authentication method (whereas in the old version, it was possible to
just ignore that function and have it fail with `Unauthorized` on
non-legacy accounts).

Each authentication method will then implement its own module, and
update all the `match` statements that match on `AuthenticatedUser`.

Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

* refactor: Rename `AuthenticatedUser::[into_]inner`

To `AuthenticatedUser::[into_]user`. More appropriate now that even the
function in `auth/mod.rs` have to use these functions.

Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

* fix: do not panic on bcrypt bug

If bcrypt for some reason fails to hash a password, return an internal
server error instead of panicking the worker thread.

Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

* elaborate on error message for UserError::NonLegacyAccount

Turns out these were only returned from `set_password`, so make the
error message more specific.

Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

* pointercrate-user: step reexporting everything at the root

Within a module, re-exporting items defined in get/post/patch/etc
submodules makes sense. Re-exporting every item in the crate at the root
does not.

Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

* chore: update gitignore with eslint stuff

Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

* fix: do not display register html if legacy_acounts is disabled

pointercrate-user/src/error.rs
@@ -71,7 +71,9 @@ pub enum UserError {
     /// for a non-legacy account).
     ///
     /// Error Code `42224`
-    #[display(fmt = "The given operation is invalid on non-legacy account")]
+    #[display(
+        fmt = "The given operation (change password) is invalid on non-legacy account, as password login is not supported for these"
+    )]
     NonLegacyAccount,
 }
 

.gitignore
@@ -3,3 +3,9 @@
 .secret
 .idea
 .vscode
+
+# random stuff I have because of enabling eslint in vscode
+node_modules
+eslint.config.mjs
+package-lock.json
+package.json

pointercrate-demonlist-api/src/endpoints/player.rs
@@ -73,7 +73,7 @@ pub async fn patch(
 
 #[rocket::put("/<player_id>/claims")]
 pub async fn put_claim(player_id: i32, mut auth: TokenAuth) -> Result<Response2<Json<PlayerClaim>>> {
-    let user_id = auth.user.inner().id;
+    let user_id = auth.user.user().id;
     let player = DatabasePlayer::by_id(player_id, &mut auth.connection).await?;
     let claim = player.initiate_claim(user_id, &mut auth.connection).await?;
 
@@ -97,7 +97,7 @@ pub async fn patch_claim(player_id: i32, user_id: i32, mut auth: TokenAuth, data
 
     let claim = match claim {
         Ok(claim) if data.lock_submissions.is_some() => {
-            if claim.user_id != auth.user.inner().id {
+            if claim.user_id != auth.user.user().id {
                 return Err(DemonlistError::ClaimNotFound {
                     member_id: user_id,
                     player_id,
@@ -164,7 +164,7 @@ pub async fn geolocate_nationality(
     player_id: i32, ip: IpAddr, mut auth: TokenAuth, ratelimits: &State<DemonlistRatelimits>,
 ) -> Result<Json<Nationality>> {
     let mut player = Player::by_id(player_id, &mut auth.connection).await?;
-    let claim = PlayerClaim::get(auth.user.inner().id, player_id, &mut auth.connection).await?;
+    let claim = PlayerClaim::get(auth.user.user().id, player_id, &mut auth.connection).await?;
 
     if !claim.verified {
         return Err(DemonlistError::ClaimUnverified.into());

pointercrate-demonlist-api/src/endpoints/record.rs
@@ -41,7 +41,7 @@ pub async fn paginate(mut auth: TokenAuth, query: Query<RecordPagination>) -> Re
         auth.require_permission(LIST_MODERATOR)?;
     }
 
-    let claim = PlayerClaim::by_user(auth.user.inner().id, &mut auth.connection)
+    let claim = PlayerClaim::by_user(auth.user.user().id, &mut auth.connection)
         .await?
         .filter(|c| c.verified);
 
@@ -84,7 +84,7 @@ pub async fn submit(
     let submission = submission.0;
     let status_is_submitted = submission.status() == RecordStatus::Submitted;
     let (is_team_member, user_id) = match auth {
-        Some(ref auth) => (auth.has_permission(LIST_HELPER), Some(auth.user.inner().id)),
+        Some(ref auth) => (auth.has_permission(LIST_HELPER), Some(auth.user.user().id)),
         None => (false, None),
     };
 
@@ -262,7 +262,7 @@ pub async fn get_notes(record_id: i32, mut auth: TokenAuth) -> Result<Response2<
     let notes = if auth.has_permission(LIST_HELPER) {
         notes_on(record_id, false, &mut auth.connection).await?
     } else {
-        match PlayerClaim::get(auth.user.inner().id, record_holder_id, &mut auth.connection).await {
+        match PlayerClaim::get(auth.user.user().id, record_holder_id, &mut auth.connection).await {
             Ok(claim) if claim.verified => notes_on(record_id, true, &mut auth.connection).await?,
             Ok(_) | Err(DemonlistError::ClaimNotFound { .. }) => return Err(DemonlistError::RecordNotFound { record_id }.into()),
             Err(err) => return Err(err.into()),
@@ -280,7 +280,7 @@ pub async fn add_note(record_id: i32, mut auth: TokenAuth, data: Json<NewNote>) 
 
     let mut note = Note::create_on(&record, data.0, &mut auth.connection).await?;
 
-    note.author = Some(auth.user.into_inner().name);
+    note.author = Some(auth.user.into_user().name);
 
     let note_id = note.id;
 
@@ -295,7 +295,7 @@ pub async fn add_note(record_id: i32, mut auth: TokenAuth, data: Json<NewNote>) 
 pub async fn patch_note(record_id: i32, note_id: i32, mut auth: TokenAuth, patch: Json<PatchNote>) -> Result<Tagged<Note>> {
     let note = Note::by_id(record_id, note_id, &mut auth.connection).await?;
 
-    if note.author.as_ref() != Some(&auth.user.inner().name) {
+    if note.author.as_ref() != Some(&auth.user.user().name) {
         auth.require_permission(LIST_ADMINISTRATOR)?;
     } else {
         auth.require_permission(LIST_HELPER)?;
@@ -312,7 +312,7 @@ pub async fn patch_note(record_id: i32, note_id: i32, mut auth: TokenAuth, patch
 pub async fn delete_note(record_id: i32, note_id: i32, mut auth: TokenAuth) -> Result<Status> {
     let note = Note::by_id(record_id, note_id, &mut auth.connection).await?;
 
-    if note.author.as_ref() != Some(&auth.user.inner().name) {
+    if note.author.as_ref() != Some(&auth.user.user().name) {
         auth.require_permission(LIST_ADMINISTRATOR)?;
     } else {
         auth.require_permission(LIST_HELPER)?;

pointercrate-demonlist-pages/Cargo.toml
@@ -18,3 +18,4 @@ chrono = "0.4.38"
 url = "2.5.2"
 async-trait = "0.1.81"
 log = "0.4.22"
+sqlx = { version = "0.8", default-features = false, features = [ "runtime-tokio-native-tls", "macros", "postgres", "chrono", "migrate" ] }

pointercrate-demonlist-pages/src/account/demons.rs
@@ -3,8 +3,9 @@ use maud::{html, Markup, PreEscaped};
 use pointercrate_core::permission::PermissionsManager;
 use pointercrate_core_pages::util::filtered_paginator;
 use pointercrate_demonlist::LIST_MODERATOR;
-use pointercrate_user::{sqlx::PgConnection, AuthenticatedUser};
+use pointercrate_user::auth::AuthenticatedUser;
 use pointercrate_user_pages::account::AccountPageTab;
+use sqlx::PgConnection;
 
 pub struct DemonsTab;
 

pointercrate-demonlist-pages/src/account/list_integration.rs
@@ -6,8 +6,9 @@ use pointercrate_core_pages::{
     util::{filtered_paginator, paginator},
 };
 use pointercrate_demonlist::player::claim::PlayerClaim;
-use pointercrate_user::{sqlx::PgConnection, AuthenticatedUser, MODERATOR};
+use pointercrate_user::{auth::AuthenticatedUser, MODERATOR};
 use pointercrate_user_pages::account::AccountPageTab;
+use sqlx::PgConnection;
 
 pub struct ListIntegrationTab(#[doc = "discord invite url"] pub &'static str);
 
@@ -36,10 +37,10 @@ impl AccountPageTab for ListIntegrationTab {
     }
 
     async fn content(&self, user: &AuthenticatedUser, permissions: &PermissionsManager, connection: &mut PgConnection) -> Markup {
-        let player_claim = match PlayerClaim::by_user(user.inner().id, connection).await {
+        let player_claim = match PlayerClaim::by_user(user.user().id, connection).await {
             Ok(player_claim) => player_claim,
             Err(err) => {
-                error!("Error retrieving player claim of user {}: {:?}", user.inner(), err);
+                error!("Error retrieving player claim of user {}: {:?}", user.user(), err);
 
                 return ErrorFragment {
                     status: err.status_code(),
@@ -49,7 +50,7 @@ impl AccountPageTab for ListIntegrationTab {
                 .body();
             },
         };
-        let is_moderator = permissions.require_permission(user.inner().permissions, MODERATOR).is_ok();
+        let is_moderator = permissions.require_permission(user.user().permissions, MODERATOR).is_ok();
 
         html! {
             div.left {

pointercrate-demonlist-pages/src/account/players.rs
@@ -2,8 +2,9 @@ use maud::{html, Markup, PreEscaped};
 use pointercrate_core::{error::PointercrateError, permission::PermissionsManager};
 use pointercrate_core_pages::{error::ErrorFragment, util::filtered_paginator};
 use pointercrate_demonlist::{nationality::Nationality, LIST_MODERATOR};
-use pointercrate_user::{sqlx::PgConnection, AuthenticatedUser};
+use pointercrate_user::auth::AuthenticatedUser;
 use pointercrate_user_pages::account::AccountPageTab;
+use sqlx::PgConnection;
 
 pub struct PlayersPage;
 

commit 88608fe5f856c9a3181d8ac4d69b0b3c22c56c32

fix(user): disambiguate 42224 error code
`NonLegacyAccount` should have been assigned code 44234. 42224 is
already in use by `DemonlistError::UnsupportedVideoHost`.

Fixes: 15d808d
Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

pointercrate-user/src/error.rs
@@ -70,7 +70,7 @@ pub enum UserError {
     /// performed on a legacy, password-based account (for example, trying to change password
     /// for a non-legacy account).
     ///
-    /// Error Code `42224`
+    /// Error Code `42234`
     #[display(
         fmt = "The given operation (change password) is invalid on non-legacy account, as password login is not supported for these"
     )]
@@ -102,7 +102,7 @@ impl PointercrateError for UserError {
             InvalidUsername => 42202,
             InvalidPassword => 42204,
             NotYouTube => 42226,
-            NonLegacyAccount => 42224,
+            NonLegacyAccount => 42234,
         }
     }
 }