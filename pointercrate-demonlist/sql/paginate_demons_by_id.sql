stadust/pointercrate

commit ddb9c1ac55d1ac1e0e4f17d14c4b717819a13845

Added the level_id parameter to the /demons and /demons/listed endpoints (#176)
Co-authored-by: Patrick <43299462+stadust@users.noreply.github.com>

pointercrate-demonlist/sql/paginate_demons_by_id.sql
@@ -15,5 +15,6 @@ WHERE (demons.id < $1 OR $1 IS NULL)
   AND (publishers.id = $9 OR $9 IS NULL)
   AND (publishers.name::CITEXT = $10 OR $10 IS NULL)
   AND (STRPOS(demons.name, $11::CITEXT) > 0 OR $11 is NULL)
+  AND (demons.level_id = $12 OR $12 IS NULL)
 ORDER BY demons.id {}
 LIMIT $12

pointercrate-demonlist/sql/paginate_demons_by_position.sql
@@ -15,6 +15,7 @@ WHERE (demons.position < $1 OR $1 IS NULL)
   AND (publishers.id = $9 OR $9 IS NULL)
   AND (publishers.name::CITEXT = $10 OR $10 IS NULL)
   AND (STRPOS(demons.name, $11::CITEXT) > 0 OR $11 is NULL)
+  AND (demons.level_id = $12 OR $12 IS NULL)
   AND demons.position IS NOT NULL
 ORDER BY demons.position {}
 LIMIT $12

pointercrate-demonlist/src/demon/paginate.rs
@@ -36,6 +36,9 @@ pub struct DemonIdPagination {
     publisher_name: Option<String>,
 
     #[serde(default, deserialize_with = "non_nullable")]
+    level_id: Option<i64>,
+
+    #[serde(default, deserialize_with = "non_nullable")]
     #[serde(rename = "requirement__gt")]
     requirement_gt: Option<i16>,
 
@@ -78,6 +81,7 @@ impl Paginatable<DemonIdPagination> for Demon {
             .bind(query.publisher_id)
             .bind(query.publisher_name.as_deref())
             .bind(query.name_contains.as_deref())
+            .bind(query.level_id)
             .bind(query.params.limit + 1)
             .fetch(connection);
 
@@ -144,6 +148,9 @@ pub struct DemonPositionPagination {
     pub publisher_name: Option<String>,
 
     #[serde(default, deserialize_with = "non_nullable")]
+    pub level_id: Option<i64>,
+
+    #[serde(default, deserialize_with = "non_nullable")]
     #[serde(rename = "requirement__gt")]
     pub requirement_gt: Option<i16>,
 
@@ -186,6 +193,7 @@ impl Paginatable<DemonPositionPagination> for Demon {
             .bind(query.publisher_id)
             .bind(query.publisher_name.as_deref())
             .bind(query.name_contains.as_deref())
+            .bind(query.level_id)
             .bind(query.params.limit + 1)
             .fetch(connection);
 

commit 148330e8f21912345e4f6f627942b48e4a7ec75a

fix(demonlist): /demons/ paginatoni endpoints ignore limit parameter
The limit query parameter was bound to the `level_id` parameter instead of the
`limit` parameter.

Fixes: ddb9c1ac55d1ac1e0e4f17d14c4b717819a13845
Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

pointercrate-demonlist/sql/paginate_demons_by_id.sql
@@ -17,4 +17,4 @@ WHERE (demons.id < $1 OR $1 IS NULL)
   AND (STRPOS(demons.name, $11::CITEXT) > 0 OR $11 is NULL)
   AND (demons.level_id = $12 OR $12 IS NULL)
 ORDER BY demons.id {}
-LIMIT $12
+LIMIT $13

pointercrate-demonlist/sql/paginate_demons_by_position.sql
@@ -18,4 +18,4 @@ WHERE (demons.position < $1 OR $1 IS NULL)
   AND (demons.level_id = $12 OR $12 IS NULL)
   AND demons.position IS NOT NULL
 ORDER BY demons.position {}
-LIMIT $12
+LIMIT $13