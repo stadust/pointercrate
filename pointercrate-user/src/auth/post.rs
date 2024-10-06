a68c1191b251e99a6c937c0fd7d409e3c06021ca
stadust <43299462+stadust@users.noreply.github.com>
2024-10-06 19:24:36 +0100

    test: Validate that token invalidation works
    Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

--- pointercrate-user/src/auth/post.rs
+++ pointercrate-user/src/auth/post.rs
@@ -21,3 +21,30 @@ impl AuthenticatedUser {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    // this is fine, as tests are always ran with --all-features
+    #[cfg(feature = "legacy_accounts")]
+    #[sqlx::test(migrations = "../migrations")]
+    fn test_invalidate_all_tokens(mut conn: sqlx::pool::PoolConnection<sqlx::Postgres>) {
+        use crate::auth::{legacy::Registration, AuthenticatedUser};
+
+        let registration = Registration {
+            name: "Patrick".to_string(),
+            password: "very bad password".to_string(),
+        };
+
+        let patrick = AuthenticatedUser::register(registration, &mut conn).await.unwrap();
+        let patricks_id = patrick.user().id;
+        let patricks_clone = AuthenticatedUser::by_id(patricks_id, &mut conn).await.unwrap();
+
+        let access_token = patrick.generate_programmatic_access_token();
+        assert!(patricks_clone.validate_programmatic_access_token(&access_token).is_ok()); // sanity check
+
+        patrick.invalidate_all_tokens(&mut conn).await.unwrap();
+
+        let patricks_clone = AuthenticatedUser::by_id(patricks_id, &mut conn).await.unwrap();
+        assert!(patricks_clone.validate_programmatic_access_token(&access_token).is_err());
+    }
+}
