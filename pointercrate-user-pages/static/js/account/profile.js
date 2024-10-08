94485f86d82e11ca0f33055ae5390f85ccc73b80
stadust <43299462+stadust@users.noreply.github.com>
2024-10-08 18:52:40 +0100

    Do not require password for account changes
    Since we differentiate between API tokens and session tokens now, and
    session tokens can perform account actions, remove all the password
    prompts (unless you're trying to actually change your password). This is
    because once we have non-legacy accounts, no password will exist, and
    we'll need to trust the browser authentication anyway.
    
    Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

--- pointercrate-user-pages/static/js/account/profile.js
+++ pointercrate-user-pages/static/js/account/profile.js
@@ -15,49 +15,17 @@ import {
 } from "/static/core/js/modules/form.js?v=4";
 
 function setupGetAccessToken() {
+  var getTokenForm = new Form(document.getElementById("get-token-form"));
   var accessTokenArea = document.getElementById("token-area");
   var accessToken = document.getElementById("access-token");
-  var getTokenButton = document.getElementById("get-token");
 
-  var htmlLoginForm = document.getElementById("login-form");
-  var loginForm = new Form(htmlLoginForm);
-
-  getTokenButton.addEventListener(
-    "click",
-    () => {
-      getTokenButton.style.display = "none";
-      accessTokenArea.style.display = "none";
-      htmlLoginForm.style.display = "block";
-    },
-    false
-  );
-
-  var loginPassword = loginForm.input("login-password");
-
-  loginPassword.clearOnInvalid = true;
-  loginPassword.addValidators({
-    "Password required": valueMissing,
-    "Password too short. It needs to be at least 10 characters long.": tooShort,
-  });
-
-  loginForm.onSubmit(function () {
-    post("/api/v1/auth/", {
-      Authorization:
-        "Basic " + btoa(window.username + ":" + loginPassword.value),
-    })
+  getTokenForm.onSubmit(function () {
+    post("/api/v1/auth/", {})
       .then((response) => {
-        loginPassword.value = "";
         accessToken.innerText = response.data.token;
-        htmlLoginForm.style.display = "none";
         accessTokenArea.style.display = "block";
       })
-      .catch((response) => {
-        if (response.data.code == 40100) {
-          loginPassword.errorText = "Invalid credentials";
-        } else {
-          loginForm.setError(response.data.message);
-        }
-      });
+      .catch(displayError(getTokenForm));
   });
 }
 
@@ -75,10 +43,10 @@ class ProfileEditorBackend extends EditorBackend {
   }
 
   headers() {
-    return {
-      "If-Match": window.etag,
-      Authorization: "Basic " + btoa(window.username + ":" + this._pw.value),
-    };
+    let headers = {"If-Match": window.etag}
+    if (this._pw) 
+      headers["Authorization"] = "Basic " + btoa(window.username + ":" + this._pw.value);
+    return headers
   }
 
   onSuccess(response) {
@@ -105,24 +73,15 @@ class ProfileEditorBackend extends EditorBackend {
 function setupEditAccount() {
   let output = new Output(document.getElementById("things"));
 
-  let editDisplayNameForm = setupFormDialogEditor(
-    new ProfileEditorBackend(document.querySelector("#auth-dn input")), // not pretty, but oh well
+  setupFormDialogEditor(
+    new ProfileEditorBackend(null),
     "edit-dn-dialog",
     "display-name-pen",
     output
   );
 
-  editDisplayNameForm.addValidators({
-    "auth-dn": {
-      "Password required": valueMissing,
-      "Password too short. It needs to be at least 10 characters long.": tooShort,
-    },
-  });
-
-  editDisplayNameForm.addErrorOverride(40100, "auth-dn");
-
   let editYoutubeForm = setupFormDialogEditor(
-    new ProfileEditorBackend(document.querySelector("#auth-yt input")), // not pretty, but oh well
+    new ProfileEditorBackend(null),
     "edit-yt-dialog",
     "youtube-pen",
     output
@@ -132,14 +91,10 @@ function setupEditAccount() {
     "edit-yt": {
       "Please enter a valid URL": typeMismatch,
     },
-    "auth-yt": {
-      "Password required": valueMissing,
-      "Password too short. It needs to be at least 10 characters long.": tooShort,
-    },
   });
 
-  editYoutubeForm.addErrorOverride(40100, "auth-yt");
   editYoutubeForm.addErrorOverride(42225, "edit-yt");
+  editYoutubeForm.addErrorOverride(42226, "edit-yt");
 
   if (document.getElementById("change-password")) {
     let changePasswordForm = setupFormDialogEditor(
@@ -176,18 +131,9 @@ function setupEditAccount() {
     $(deleteAccountDialog.parentElement).show();
   });
 
-  var deleteAuth = deleteAccountForm.input("auth-delete");
-  deleteAuth.addValidators({
-    "Password required": valueMissing,
-    "Password too short. It needs to be at least 10 characters long.": tooShort,
-  });
-
-  deleteAccountForm.addErrorOverride(40100, "auth-delete");
-
   deleteAccountForm.onSubmit(() => {
     del("/api/v1/auth/me/", {
       "If-Match": window.etag,
-      Authorization: "Basic " + btoa(window.username + ":" + deleteAuth.value),
     })
       .then(() => window.location.reload())
       .catch(displayError(deleteAccountForm));
@@ -195,36 +141,11 @@ function setupEditAccount() {
 }
 
 function setupInvalidateToken() {
-  var invalidateButton = document.getElementById("invalidate-token");
   var htmlInvalidateForm = document.getElementById("invalidate-form");
   var invalidateForm = new Form(htmlInvalidateForm);
 
-  invalidateButton.addEventListener(
-    "click",
-    () => {
-      invalidateButton.style.display = "none";
-      htmlInvalidateForm.style.display = "block";
-    },
-    false
-  );
-
-  var invalidatePassword = invalidateForm.input("invalidate-auth-password");
-
-  invalidatePassword.clearOnInvalid = true;
-  invalidateForm.addValidators({
-    "invalidate-auth-password": {
-      "Password required": valueMissing,
-      "Password too short. It needs to be at least 10 characters long.": tooShort,
-    },
-  });
-
-  invalidateForm.addErrorOverride(40100, "invalidate-auth-password");
-
   invalidateForm.onSubmit(function () {
-    post("/api/v1/auth/invalidate/", {
-      Authorization:
-        "Basic " + btoa(window.username + ":" + invalidatePassword.value),
-    })
+    post("/api/v1/auth/invalidate/")
       .then(() => window.location.reload())
       .catch(displayError(invalidateForm));
   });
