2737a2f33c0d5cb3cb3b00d077989b5f850df4c9
stadust <43299462+stadust@users.noreply.github.com>
2025-03-08 11:38:59 +0000

    user-page: center login promp vertically
    Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

--- pointercrate-user-pages/src/login.rs
+++ pointercrate-user-pages/src/login.rs
@@ -46,36 +46,38 @@ fn login_page_body() -> Markup {
         html!()
     };
     html! {
-        div.m-center.flex.panel.fade.col.wrap style = "margin: 100px 0px;"{
-            h1.underlined.pad {
-                "Pointercrate Account"
-            }
-            p {
-                "By using pointercrate accounts you agree to cookies. If you don't then I formally request you to stop using the internet as you obviously have no idea what you're talking about. "
-            }
-            div.flex #login {
-                div.flex.col {
-                    h2 {"Login"}
-                    p {
-                        "Log in to an existing pointercrate account. You have 3 login attempts by 30 minutes. If you do not have an account yet, register on the right or below. "
-                    }
-                    form.flex.col.grow #login-form novalidate = "" {
-                        p.info-red.output {}
-                        span.form-input #login-username {
-                            label for = "username" {"Username:"}
-                            input required = "" type = "text" name = "username" minlength = "3";
-                            p.error {}
-                        }
-                        span.form-input #login-password {
-                            label for = "password" {"Password:"}
-                            input required = "" type = "password" name = "password" minlength = "10";
-                            p.error {}
-                        }
-                        div.grow {}
-                        input.button.blue.hover type = "submit" style = "margin: 15px auto 0px;" value="Log in";
-                    }
+        div style="display: flex; align-items: center; justify-content: center; height: calc(100% - 70px)" { // 70px = height of nav bar
+            div.m-center.panel.fade {
+                h1.underlined.pad {
+                    "Pointercrate Account"
                 }
-                (legacy_register)
+                p {
+                    "By using pointercrate accounts you agree to cookies. If you don't then I formally request you to stop using the internet as you obviously have no idea what you're talking about. "
+                }
+                div.flex #login {
+                    div.flex.col {
+                        h2 {"Login"}
+                        p {
+                            "Log in to an existing pointercrate account. You have 3 login attempts by 30 minutes. If you do not have an account yet, register on the right or below. "
+                        }
+                        form.flex.col.grow #login-form novalidate = "" {
+                            p.info-red.output {}
+                            span.form-input #login-username {
+                                label for = "username" {"Username:"}
+                                input required = "" type = "text" name = "username" minlength = "3";
+                                p.error {}
+                            }
+                            span.form-input #login-password {
+                                label for = "password" {"Password:"}
+                                input required = "" type = "password" name = "password" minlength = "10";
+                                p.error {}
+                            }
+                            div.grow {}
+                            input.button.blue.hover type = "submit" style = "margin: 15px auto 0px;" value="Log in";
+                        }
+                    }
+                    (legacy_register)
+                }
             }
         }
     }
