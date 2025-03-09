6f6500e20b90ced0176178cffadc35540b8d4d49
stadust <43299462+stadust@users.noreply.github.com>
2025-03-09 13:55:57 +0000

    Handle members with linked google accounts in auth module
    Introduce a new `AuthenticationType` for these, so that password based
    login will automatically be rejected.
    
    Signed-off-by: stadust <43299462+stadust@users.noreply.github.com>

--- pointercrate-user/src/auth/oauth/mod.rs
+++ pointercrate-user/src/auth/oauth/mod.rs
@@ -2,3 +2,27 @@ mod post;
 
 #[cfg(feature = "oauth2")]
 pub use post::GoogleOauthPayload;
+
+use crate::User;
+
+use super::AuthenticationType;
+
+pub struct OA2AuthenticatedUser {
+    user: User,
+}
+
+impl OA2AuthenticatedUser {
+    pub fn into_user(self) -> User {
+        self.user
+    }
+
+    pub fn user(&self) -> &User {
+        &self.user
+    }
+}
+
+impl AuthenticationType {
+    pub fn oauth(user: User) -> AuthenticationType {
+        AuthenticationType::Oauth2(OA2AuthenticatedUser { user })
+    }
+}
