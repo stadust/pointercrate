0f0e2fc5f908118625de9e88ca33a6af6353bb01
jasonyess <jasonyes.gd@gmail.com>
2025-03-27 21:07:18 -0400

    user: localization support for submitters tab in user area

---
+++ locales/en/submitter.ftl
@@ -0,0 +1,24 @@
+submitter-banned = Banned
+    .yes = Yes
+    .no = No
+
+## Submitters tab
+submitters = Submitters
+
+submitter-manager = Submitter Manager
+
+submitter-viewer = Submitter #
+    .welcome = Click on a submitter on the left to get started!
+
+    .info-a = Welcome to the submitter manager. Here you can ban or unban submitters with an absolute revolutionary UI that totally isn't a straight up copy of the player UI, just with even more emptiness.
+    .info-b = Banning a submitter will delete all records they have submitted and which are still in the 'submitted' state. All submissions of their which are approved, rejected or under consideration are untouched.
+
+    .records-redirect = Show records in record manager
+
+submitter-listed = Submitter #{$submitterId}
+
+submitter-idsearch-panel = Search submitter by ID
+    .info = Submitters can be uniquely identified by ID. Entering a submitters's ID below will select it on the left (provided the submitter exists)
+
+    .id-field = Submitter ID
+    .submit = Find by ID
--- pointercrate-demonlist-pages/src/account/submitters.rs
+++ pointercrate-demonlist-pages/src/account/submitters.rs
@@ -1,10 +1,11 @@
 use maud::{html, Markup, PreEscaped};
-use pointercrate_core::permission::PermissionsManager;
+use pointercrate_core::{localization::tr, permission::PermissionsManager};
 use pointercrate_core_pages::util::paginator;
 use pointercrate_demonlist::LIST_MODERATOR;
 use pointercrate_user::auth::{AuthenticatedUser, NonMutating};
 use pointercrate_user_pages::account::AccountPageTab;
 use sqlx::PgConnection;
+use unic_langid::LanguageIdentifier;
 
 pub struct SubmittersPage;
 
@@ -22,10 +23,10 @@ impl AccountPageTab for SubmittersPage {
         6
     }
 
-    fn tab(&self) -> Markup {
+    fn tab(&self, lang: &'static LanguageIdentifier) -> Markup {
         html! {
             b {
-                "Submitters"
+                (tr(lang, "submitters"))
             }
             (PreEscaped("&nbsp;&nbsp;"))
             i class = "fa fa-eye fa-2x" aria-hidden="true" {}
@@ -33,37 +34,38 @@ impl AccountPageTab for SubmittersPage {
     }
 
     async fn content(
-        &self, _user: &AuthenticatedUser<NonMutating>, _permissions: &PermissionsManager, _connection: &mut PgConnection,
+        &self, lang: &'static LanguageIdentifier, _user: &AuthenticatedUser<NonMutating>, _permissions: &PermissionsManager,
+        _connection: &mut PgConnection,
     ) -> Markup {
         html! {
             div.left {
                 div.panel.fade {
                     h2.underlined.pad {
-                        "Submitter Manager"
+                        (tr(lang, "submitter-manager"))
                     }
                     div.flex.viewer {
                         (paginator("submitter-pagination", "/api/v1/submitters/"))
                         p.viewer-welcome {
-                            "Click on a submitter on the left to get started!"
+                            (tr(lang, "submitter-viewer.welcome"))
                         }
                         div.viewer-content {
                             div.flex.col{
                                 h3 style = "font-size:1.1em; margin: 10px 0" {
-                                    "Submitter #"
+                                    (tr(lang, "submitter-viewer"))
                                     i #submitter-submitter-id {}
                                 }
                                 p {
-                                    "Welcome to the submitter manager. Here you can ban or unban submitters with an absolute revolutionary UI that totally isn't a stright up copy of the player UI, just with even more emptiness. "
+                                    (tr(lang, "submitter-viewer.info-a"))
                                 }
                                 p {
-                                    "Banning a submitter will delete all records they have submitted and which are still in the 'submitted' state. All submissions of their which are approved, rejected or under consideration are untouched. "
+                                    (tr(lang, "submitter-viewer.info-b"))
                                 }
                                 p.info-red.output style = "margin: 10px" {}
                                 p.info-green.output style = "margin: 10px" {}
                                 div.stats-container.flex.space {
                                     span {
                                         b {
-                                            "Banned:"
+                                            (tr(lang, "submitter-banned")) ":"
                                         }
                                         br;
                                         div.dropdown-menu.js-search #edit-submitter-banned style = "max-width: 50px" {
@@ -72,14 +74,14 @@ impl AccountPageTab for SubmittersPage {
                                             }
                                             div.menu {
                                                 ul {
-                                                    li.white.hover data-value="true" {"yes"}
-                                                    li.white.hover data-value="false" {"no"}
+                                                    li.white.hover data-value="true" {(tr(lang, "submitter-banned.yes"))}
+                                                    li.white.hover data-value="false" {(tr(lang, "submitter-banned.no"))}
                                                 }
                                             }
                                         }
                                     }
                                 }
-                                span.button.blue.hover #submitter-list-records style = "margin: 15px auto 0px" {"Show records in record manager"};
+                                span.button.blue.hover #submitter-list-records style = "margin: 15px auto 0px" {(tr(lang, "submitter-viewer.records-redirect"))};
                             }
                         }
                     }
@@ -87,29 +89,29 @@ impl AccountPageTab for SubmittersPage {
                 div style="height: 50px" {} // to make sure that the footer doesnt float. if it floats, the user page is the only one without a scrollbar at the right, which causes jumpyness when switching tabs.
             }
             div.right {
-                (submitter_selector())
+                (submitter_selector(lang))
             }
         }
     }
 }
 
-fn submitter_selector() -> Markup {
+fn submitter_selector(lang: &'static LanguageIdentifier) -> Markup {
     html! {
         div.panel.fade {
             h2.underlined.pad {
-                "Search submitter by ID"
+                (tr(lang, "submitter-idsearch-panel"))
             }
             p {
-                "Submitters can be uniquely identified by ID. Entering a submitters's ID below will select it on the left (provided the submitter exists)"
+                (tr(lang, "submitter-idsearch-panel.info"))
             }
             form.flex.col #submitter-search-by-id-form novalidate = "" {
                 p.info-red.output {}
                 span.form-input #search-submitter-id {
-                    label for = "id" {"Submitter ID:"}
+                    label for = "id" {(tr(lang, "submitter-idsearch-panel.id-field")) ":"}
                     input required = "" type = "number" name = "id" min = "0" style="width:93%";
                     p.error {}
                 }
-                input.button.blue.hover type = "submit" style = "margin: 15px auto 0px;" value="Find by ID";
+                input.button.blue.hover type = "submit" style = "margin: 15px auto 0px;" value=(tr(lang, "submitter-idsearch-panel.submit"));
             }
         }
     }
