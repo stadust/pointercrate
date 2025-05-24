cd55cfc4b97d8da697f186146a48ff57a3276517
jasonyess <jasonyes.gd@gmail.com>
2025-05-24 18:41:09 -0400

    update example binary

--- pointercrate-example/src/main.rs
+++ pointercrate-example/src/main.rs
@@ -1,7 +1,10 @@
+use std::sync::Arc;
+
 use maud::html;
 use pointercrate_core::error::CoreError;
 use pointercrate_core::pool::PointercratePool;
 use pointercrate_core_api::{error::ErrorResponder, maintenance::MaintenanceFairing, preferences::PreferenceManager};
+use pointercrate_core_macros::localized_catcher;
 use pointercrate_core_pages::{
     footer::{Footer, FooterColumn, Link},
     localization::LocalizationConfiguration,
@@ -21,8 +24,9 @@ use rocket::{fs::FileServer, response::Redirect, uri};
 ///
 /// An [`ErrorResponder`] will return either a JSON or an HTML error page,
 /// depending on what `Accept` headers are set on the request.
+#[localized_catcher]
 #[rocket::catch(404)]
-fn catch_404() -> ErrorResponder {
+async fn catch_404() -> ErrorResponder {
     // `CoreError` contains various generic error conditions that might happen
     // anywhere across the website. `CoreError::NotFound` is a generic 404 NOT FOUND
     // error with code 40400.
@@ -32,14 +36,16 @@ fn catch_404() -> ErrorResponder {
 /// Failures in json deserialization of request bodies will just return
 /// an immediate 422 response. This catcher is needed to translate them into a pointercrate
 /// error response.
+#[localized_catcher]
 #[rocket::catch(422)]
-fn catch_422() -> ErrorResponder {
+async fn catch_422() -> ErrorResponder {
     CoreError::UnprocessableEntity.into()
 }
 
 /// Failures from the authorization FromRequest implementations can return 401s
+#[localized_catcher]
 #[rocket::catch(401)]
-fn catch_401() -> ErrorResponder {
+async fn catch_401() -> ErrorResponder {
     CoreError::Unauthorized.into()
 }
 
@@ -63,7 +69,7 @@ async fn rocket() -> _ {
         // Tell it about the connection pool to use (individual handlers can get hold of this pool by declaring an argument of type `&State<PointercratePool>`)
         .manage(pool)
         // Tell pointercrate's core components about navigation bar and footers, so that it knows how to render the website
-        .manage(page_configuration())
+        .manage(Arc::new(page_configuration as fn() -> PageConfiguration))
         // Register our 404 catcher
         .register("/", rocket::catchers![catch_401, catch_404, catch_422])
         // Register our home page
@@ -142,7 +148,8 @@ async fn rocket() -> _ {
     let rocket = rocket
         .mount("/static/core", FileServer::from("pointercrate-core-pages/static"))
         .mount("/static/demonlist", FileServer::from("pointercrate-demonlist-pages/static"))
-        .mount("/static/user", FileServer::from("pointercrate-user-pages/static"));
+        .mount("/static/user", FileServer::from("pointercrate-user-pages/static"))
+        .mount("/static/core", rocket::routes![pointercrate_core_api::localization::get_ftl]);
 
     rocket
 }
@@ -172,19 +179,17 @@ fn page_configuration() -> PageConfiguration {
                 },
             )
             // Add a drop down to the demonlist item, just like on pointercrate.com
-            .with_sub_item(Some("/demonlist/statsviewer/"), html! {"Stats Viewer"})
-            .with_sub_item(Some("/demonlist/?submitter=true"), html! {"Record Submitter"})
-            .with_sub_item(Some("/demonlist/?timemachine=true"), html! {"Time Machine"}),
+            .with_sub_item(Some("/demonlist/statsviewer/"), html! { "Stats Viewer" })
+            .with_sub_item(Some("/demonlist/?submitter=true"), html! { "Record Submitter" })
+            .with_sub_item(Some("/demonlist/?timemachine=true"), html! { "Time Machine" }),
         )
-        .with_item(TopLevelNavigationBarItem::new(
-            None,
-            Some("/login/"),
+        .with_item(TopLevelNavigationBarItem::new(None, Some("/login/"), {
             html! {
                 span {
                     "User Area"
                 }
-            },
-        ));
+            }
+        }));
 
     // A footer consists of a copyright notice, an arbitrary amount of columns
     // displayed below it, side-by-side, and potentially some social media links to
