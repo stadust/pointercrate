ff72bc40afc60d1b7438baef493053a5cbd0c6fb
jasonyess <jasonyes.gd@gmail.com>
2025-05-31 17:34:37 -0400

    fix wrong LocaleSet being retrieved on empty uris

--- pointercrate-core-pages/src/localization.rs
+++ pointercrate-core-pages/src/localization.rs
@@ -137,7 +137,10 @@ impl LocalizationConfiguration {
     pub fn set_by_uri(&self, uri: PathBuf) -> LocaleSet {
         self.overrides
             .iter()
-            .find(|(key, _)| uri.components().zip(key.components()).all(|(a, b)| a == b))
+            .find(|(key, _)| {
+                uri.components().collect::<Vec<_>>().len() >= key.components().collect::<Vec<_>>().len()
+                    && key.components().zip(uri.components()).all(|(a, b)| a == b)
+            })
             .map(|(_, locale_set)| locale_set.clone())
             .unwrap_or(self.default.clone())
     }
